787a03b7d995151e1ac943373a897cf1e86d0a57
Muharem Hrnjadovic <mh@cloud1.it>
2014-02-05 21:56:56 +0100

    split tests and refactor code

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -8,26 +8,36 @@
 """Tests related to the trie.Trie class."""
 
 
-import json
-import os
+import random
 import trie
+import utils
 
 
 class TestTrie:
-    __TESTDATADIR = "../tests"
+    def _load_data(self, td_idx):
+        # Load the test data
+        tdata = utils.load_test_data("trietest.txt")
+        tdatum = tdata[td_idx]
+        inputs = tdatum["inputs"]
+        expected = tdatum["expectation"]
+        return (inputs, expected)
 
-    def _load_test_data(self):
-        return json.loads(
-            open(os.path.join(self.__TESTDATADIR, 'trietest.txt')).read())
-
-    def test_trie(self, tmpdir):
-        db = tmpdir.ensure("trie-db", dir=True)
+    def _test_hash(self, tmpdir, td_idx):
+        # `tmpdir` is a `py.path.local` object which offers `os.path` methods,
+        # see http://pytest.org/latest/tmpdir.html for details.
+        inputs, expected = self._load_data(td_idx)
+        # Prepare the Trie
+        db = tmpdir.ensure("triedb-%s" % random.randrange(10000000), dir=True)
         t0 = trie.Trie(db.strpath)
-        tdata = self._load_test_data()
-        for tdatum in tdata:
-            inputs = tdatum["inputs"]
-            expected = tdatum["expectation"]
-            for k, v in inputs.items():
-                t0.update(k, v)
-            actual = t0.root.encode('hex')
-            assert expected == actual
+        for k, v in inputs.items():
+            t0.update(k, v)
+        # The actual test
+        actual = t0.root.encode('hex')
+        assert expected == actual, (
+            "\ninputs = '%s',\nhash = '%s'" % (inputs, actual))
+
+    def test_trie1(self, tmpdir):
+        self._test_hash(tmpdir, 0)
+
+    def test_trie2(self, tmpdir):
+        self._test_hash(tmpdir, -1)
