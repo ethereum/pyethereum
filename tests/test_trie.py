f5343462b5eff32a16f6b8bdf31eb20701aa0806
Muharem Hrnjadovic <mh@cloud1.it>
2014-02-06 08:10:20 +0100

    simplified trie.Trie tests

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -14,30 +14,20 @@ import utils
 
 
 class TestTrie:
-    def _load_data(self, td_idx):
-        # Load the test data
-        tdata = utils.load_test_data("trietest.txt")
-        tdatum = tdata[td_idx]
-        inputs = tdatum["inputs"]
-        expected = tdatum["expectation"]
-        return (inputs, expected)
-
-    def _test_hash(self, tmpdir, td_idx):
+    def test_hashes(self, tmpdir):
         # `tmpdir` is a `py.path.local` object which offers `os.path` methods,
         # see http://pytest.org/latest/tmpdir.html for details.
-        inputs, expected = self._load_data(td_idx)
-        # Prepare the Trie
-        db = tmpdir.ensure("triedb-%s" % random.randrange(10000000), dir=True)
-        t0 = trie.Trie(db.strpath)
-        for k, v in inputs.items():
-            t0.update(k, v)
-        # The actual test
-        actual = t0.root.encode('hex')
-        assert expected == actual, (
-            "\ninputs = '%s',\nhash = '%s'" % (inputs, actual))
+        tdata = utils.load_test_data("trietest.txt")
+        for tdatum in tdata:
+            inputs = tdatum["inputs"]
+            expected = tdatum["expectation"]
 
-    def test_trie1(self, tmpdir):
-        self._test_hash(tmpdir, 0)
-
-    def test_trie2(self, tmpdir):
-        self._test_hash(tmpdir, -1)
+            # Prepare the Trie
+            db = tmpdir.ensure("tdb-%s" % random.randrange(1000000), dir=True)
+            t0 = trie.Trie(db.strpath)
+            for k, v in inputs.items():
+                t0.update(k, v)
+            # The actual test
+            actual = t0.root.encode('hex')
+            assert expected == actual, (
+                "\ninputs = '%s',\nhash = '%s'" % (inputs, actual))
