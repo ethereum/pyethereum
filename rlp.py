df8c95246e8d0a006c84b654f517096af426f60e
Muharem Hrnjadovic <mh@cloud1.it>
2014-03-03 08:59:08 +0100

    fix exception clause

--- rlp.py
+++ rlp.py
@@ -53,7 +53,7 @@ def __decode(s,pos=0):
             o.append(obj)
         return (o,pos)
     else:
-        raise Exception("byte not supported: "+fchar)
+        raise RuntimeError("byte not supported: " + str(fchar))
 
 def decode(s): return __decode(s)[0]
 
