bbc75223d0638993808c1fc4778aace0323d48ad
Chen Houwu <chenhouwu@gmail.com>
2014-03-17 12:43:58 +0800

    more test scenario for rlp

--- features/rlp.feature
+++ features/rlp.feature
@@ -2,5 +2,17 @@ Feature: RLP encoding and decoding
 
   Scenario: a single byte
     Given the byte is in [0x00, 0x7f] range
+    When encoded in RLP
     Then the byte is its own RLP encoding
 
+  Scenario Outline: the string is [0-55] long
+    Given <src>
+    When encoded in RLP
+    Then the first byte is 0x80 plus the length of the string
+    And followed by the string
+
+    Examples: 0-55 string
+      | src                                        |
+      | a single byte is not in [0x00, 0x7f] range |
+      | a blank string                             |
+      | a 2-55 bytes long string                   |
--- features/steps/rlp.py
+++ features/steps/rlp.py
@@ -1,12 +1,56 @@
 from behave import *
 
+
 @given('the byte is in [0x00, 0x7f] range')
 def step_impl(ctx):
     ctx.srcs = [chr(0x00), chr(0x71), chr(0x7f)]
 
 
+@when('encoded in RLP')
+def step_impl(ctx):
+    import rlp
+    dsts = [rlp.encode(src) for src in ctx.srcs]
+    ctx.pairs = zip(ctx.srcs, dsts)
+
+
 @then('the byte is its own RLP encoding')
 def step_impl(ctx):
     import rlp
-    for src in ctx.srcs:
-        assert rlp.encode(src) == src
+    for src, dst in ctx.pairs:
+        assert dst == src
+
+
+@given('a single byte is not in [0x00, 0x7f] range')
+def step_impl(ctx):
+    ctx.srcs = [
+        chr(0x80),
+        chr(0x81),
+        chr(0xFF)
+    ]
+
+
+@given('a 2-55 bytes long string')
+def step_impl(ctx):
+    ctx.srcs = [
+        'abcd',
+        'a' * 55
+    ]
+
+
+@given('a blank string')
+def step_impl(ctx):
+    ctx.srcs = [
+        ''
+    ]
+
+
+@then('the first byte is 0x80 plus the length of the string')
+def step_impl(ctx):
+    for src, dst in ctx.pairs:
+        assert dst[0] == chr(0x80 + len(src))
+
+
+@then('followed by the string')
+def step_impl(ctx):
+    for src, dst in ctx.pairs:
+        assert dst[1:] == src
