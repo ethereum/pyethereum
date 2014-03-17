a32ed7c61be305df42ea337bc7e8393c1a8894b7
Chen Houwu <chenhouwu@gmail.com>
2014-03-17 17:26:10 +0800

    add test for list with payload of length [56-]

--- features/rlp.feature
+++ features/rlp.feature
@@ -5,6 +5,7 @@ Feature: RLP encoding and decoding
     When encoded in RLP
     Then the byte is its own RLP encoding
 
+
   Scenario Outline: payload is a [0-55] long string
     Given <src>
     When encoded in RLP
@@ -17,16 +18,25 @@ Feature: RLP encoding and decoding
       | a blank string                             |
       | a 2-55 bytes long string                   |
 
+
   Scenario: payload is a [56-] long string
     Given a string longer than 55
     When encoded in RLP
-    Then the first byte is 0xb7 plus the lenth of the lenth
+    Then the first byte is 0xb7 plus the length of the length of the string
     And following bytes are the payload string length
     And following bytes are the payload string itself
 
 
-  Scenario: payload is a list with total lenght [0-55]
+  Scenario: payload is a list with total length [0-55]
     Given a list with length of [0-55]
     When encoded in RLP
     Then the first byte is 0xc0 plus the length of the list
     And following bytes are concatenation of the RLP encodings of the items
+
+
+  Scenario: payload is a list with total length [56-]
+    Given a list with length of [56-]
+    When encoded in RLP
+    Then the first byte is 0xf7 plus the length of the length of the list
+    And following bytes are the payload list length
+    And following bytes are the payload list itself
--- features/steps/rlp.py
+++ features/steps/rlp.py
@@ -8,18 +8,15 @@ def step_impl(context):
     dsts = [rlp.encode(src) for src in context.srcs]
     context.pairs = zip(context.srcs, dsts)
 
-
 @given(u'the byte is in [0x00, 0x7f] range')
 def step_impl(context):
     context.srcs = [chr(0x00), chr(0x71), chr(0x7f)]
-
 
 @then(u'the byte is its own RLP encoding')
 def step_impl(context):
     import rlp
     for src, dst in context.pairs:
         assert dst == src
-
 
 @given(u'a single byte is not in [0x00, 0x7f] range')
 def step_impl(context):
@@ -29,7 +26,6 @@ def step_impl(context):
         chr(0xFF)
     ]
 
-
 @given(u'a 2-55 bytes long string')
 def step_impl(context):
     context.srcs = [
@@ -37,25 +33,21 @@ def step_impl(context):
         'a' * 55
     ]
 
-
 @given(u'a blank string')
 def step_impl(context):
     context.srcs = [
         ''
     ]
 
-
 @then(u'the first byte is 0x80 plus the length of the string')
 def step_impl(context):
     for src, dst in context.pairs:
         assert dst[0] == chr(0x80 + len(src))
 
-
 @then(u'followed by the string')
 def step_impl(context):
     for src, dst in context.pairs:
         assert dst[1:] == src
-
 
 @given(u'a string longer than 55')
 def step_impl(context):
@@ -64,13 +56,11 @@ def step_impl(context):
         'a' * 1024
     ]
 
-
-@then(u'the first byte is 0xb7 plus the lenth of the lenth')
+@then(u'the first byte is 0xb7 plus the length of the length of the string')
 def step_impl(context):
     for src, dst in context.pairs:
         length_bin = to_big_endian_binary(len(src))
         assert dst[0] == chr(0xb7 + len(length_bin))
-
 
 @then(u'following bytes are the payload string length')
 def step_impl(context):
@@ -78,13 +68,11 @@ def step_impl(context):
         length_bin = to_big_endian_binary(len(src))
         assert dst[1:1+len(length_bin)] == length_bin
 
-
 @then(u'following bytes are the payload string itself')
 def step_impl(context):
     for src, dst in context.pairs:
         length_bin = to_big_endian_binary(len(src))
         assert dst[1+len(length_bin):] == src
-
 
 @given(u'a list with length of [0-55]')
 def step_impl(context):
@@ -94,7 +82,6 @@ def step_impl(context):
         ['a'] * 55,
     ]
 
-
 @then(u'the first byte is 0xc0 plus the length of the list')
 def step_impl(context):
     import rlp
@@ -102,9 +89,40 @@ def step_impl(context):
         total_payload_length = sum(len(rlp.encode(x)) for x in src)
         assert dst[0] == chr(0xc0 + total_payload_length)
 
-
 @then(u'following bytes are concatenation of the RLP encodings of the items')
 def step_impl(context):
     import rlp
     for src, dst in context.pairs:
         assert dst[1:] == ''.join(rlp.encode(x) for x in src)
+
+@given(u'a list with length of [56-]')
+def step_impl(context):
+    context.srcs = [
+        ['a'] * 56,
+        ['a'] * 1024,
+    ]
+
+@then(u'the first byte is 0xf7 plus the length of the length of the list')
+def step_impl(context):
+    import rlp
+    for src, dst in context.pairs:
+        total_payload_length = sum(len(rlp.encode(x)) for x in src)
+        length_bin = to_big_endian_binary(total_payload_length)
+        assert dst[0] == chr(0xf7 + len(length_bin))
+
+@then(u'following bytes are the payload list length')
+def step_impl(context):
+    import rlp
+    for src, dst in context.pairs:
+        total_payload_length = sum(len(rlp.encode(x)) for x in src)
+        length_bin = to_big_endian_binary(total_payload_length)
+        assert dst[1:1+len(length_bin)] == length_bin
+
+@then(u'following bytes are the payload list itself')
+def step_impl(context):
+    import rlp
+    for src, dst in context.pairs:
+        encodeds = [rlp.encode(x) for x in src]
+        total_payload_length = sum(len(x) for x in encodeds)
+        length_bin = to_big_endian_binary(total_payload_length)
+        assert dst[1+len(length_bin):] == ''.join(encodeds)
