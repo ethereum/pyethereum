2881c2f04f0661b713ece1136e4cf40ad86c3367
Chen Houwu <chenhouwu@gmail.com>
2014-03-18 20:23:31 +0800

    1. finish: test codes for compat encoding 2. bug: compat encoding(find by running test, but not fixed)

--- features/compact_encoding.feature
+++ features/compact_encoding.feature
@@ -15,11 +15,11 @@ Feature: Compact encoding of hex sequence with optional terminator
     And decode the compactly encoded hex sequence will get the original one
 
   Scenario: Even length hex sequence, with terminator
-    Given an odd length hex sequence
+    Given an Even length hex sequence
     When append a terminator
     And compactly encoded
     And remove terminator from source
-    Then the first byte should start with 0x2
+    Then the first byte should be 0x20
     And the remain bits will be same of the original hex sequence
     And decode the compactly encoded hex sequence will get the original one
 
--- features/steps/compact_encoding.py
+++ features/steps/compact_encoding.py
@@ -6,6 +6,18 @@ def step_impl(context):
         [0x1, 0x2, 0x3, 0x4]
     ]
 
+@given(u'an odd length hex sequence')
+def step_impl(context):
+    context.srcs = [
+        [0x0],
+        [0x1, 0x2, 0x3]
+    ]
+
+@when(u'append a terminator')
+def step_impl(context):
+    for src in context.srcs:
+        src.append(16)
+
 @when(u'compactly encoded')
 def step_impl(context):
     import trie
@@ -14,21 +26,45 @@ def step_impl(context):
         for src in context.srcs
     ]
 
+@when(u'remove terminator from source')
+def step_impl(context):
+    for src in context.srcs:
+        del src[-1]
+
 @then(u'the first byte should be 0x00')
 def step_impl(context):
     for src, dst in context.pairs:
         assert ord(dst[0]) == 0
     context.prefex_hex_count = 2
 
+@then(u'the first byte should start with 0x1')
+def step_impl(context):
+    for src, dst in context.pairs:
+        assert ord(dst[0]) & 0xF0 == 0x10
+    context.prefex_hex_count = 1
+
+@then(u'the first byte should be 0x20')
+def step_impl(context):
+    for src, dst in context.pairs:
+        assert ord(dst[0]) & 0xF0 == 0x20
+    context.prefex_hex_count = 2
+
+@then(u'the first byte should start with 0x3')
+def step_impl(context):
+    for src, dst in context.pairs:
+        assert ord(dst[0]) & 0xF0 == 0x30
+    context.prefex_hex_count = 1
+
 @then(u'the remain bits will be same of the original hex sequence')
 def step_impl(context):
     for src, dst in context.pairs:
-        assert context.prefex_hex_count in [1, 2]
-        assert len(src) == len(dst) - context.prefex_hex_count
+        print src, len(dst), ord(dst[0])
+        assert len(src) == len(dst)*2 - context.prefex_hex_count
 
         dst_hexes = []
+
         for byte in dst:
-            dst_hex.extend([ord(byte) & 0xF0, ord(byte) & 0x0F])
+            dst_hexes.extend([ord(byte) >> 4, ord(byte) & 0x0F])
 
         for src_hex, dst_hex in zip(src, dst_hexes[context.prefex_hex_count:]):
             assert src_hex == dst_hex
@@ -40,38 +76,3 @@ def step_impl(context):
         decoded_hexes = trie.bin_to_hexarraykey(dst)
         for src_hex, dst_hex in zip(src, decoded_hexes):
             assert src_hex == dst_hex
-
-@given(u'an odd length hex sequence')
-def step_impl(context):
-    context.srcs = [
-        [0x0],
-        [0x1, 0x2, 0x3]
-    ]
-
-@then(u'the first byte should start with 0x1')
-def step_impl(context):
-    for src, dst in context.pairs:
-        assert ord(dst[0]) & 0xF0 == 0x10
-    context.prefex_hex_count = 1
-
-@when(u'append a terminator')
-def step_impl(context):
-    for src in context.srcs:
-        src.append(16)
-
-@when(u'remove terminator from source')
-def step_impl(context):
-    for src in context.srcs:
-        del src[-1]
-
-@then(u'the first byte should start with 0x2')
-def step_impl(context):
-    for src, dst in context.pairs:
-        assert ord(dst[0]) & 0xF0 == 0x20
-    context.prefex_hex_count = 1
-
-@then(u'the first byte should start with 0x3')
-def step_impl(context):
-    for src, dst in context.pairs:
-        assert ord(dst[0]) & 0xF0 == 0x30
-    context.prefex_hex_count = 1
