a206850899b9188831fdfa62e578b6c17625900f
Chen Houwu <chenhouwu@gmail.com>
2014-03-18 18:23:52 +0800

    full test codes for compact encoding, need to be modifed later

--- features/compact_encoding.feature
+++ features/compact_encoding.feature
@@ -4,28 +4,30 @@ Feature: Compact encoding of hex sequence with optional terminator
     Given an Even length hex sequence
     When compactly encoded
     Then the first byte should be 0x00
-    And the remain bits with be same of the original hex sequence
+    And the remain bits will be same of the original hex sequence
     And decode the compactly encoded hex sequence will get the original one
 
   Scenario: Odd length hex sequence, without terminator
     Given an odd length hex sequence
     When compactly encoded
     Then the first byte should start with 0x1
-    And the remain bits with be same of the original hex sequence
+    And the remain bits will be same of the original hex sequence
     And decode the compactly encoded hex sequence will get the original one
 
   Scenario: Even length hex sequence, with terminator
     Given an odd length hex sequence
     When append a terminator
     And compactly encoded
+    And remove terminator from source
     Then the first byte should start with 0x2
-    And the remain bits with be same of the original hex sequence
+    And the remain bits will be same of the original hex sequence
     And decode the compactly encoded hex sequence will get the original one
 
   Scenario: Odd length hex sequence, with terminator
     Given an odd length hex sequence
     When append a terminator
     And compactly encoded
+    And remove terminator from source
     Then the first byte should start with 0x3
-    And the remain bits with be same of the original hex sequence
+    And the remain bits will be same of the original hex sequence
     And decode the compactly encoded hex sequence will get the original one
--- features/steps/compact_encoding.py
+++ features/steps/compact_encoding.py
@@ -1,8 +1,8 @@
 @given(u'an Even length hex sequence')
 def step_impl(context):
     context.srcs = [
-        []
-        [0x0, 0x1]
+        [],
+        [0x0, 0x1],
         [0x1, 0x2, 0x3, 0x4]
     ]
 
@@ -17,33 +17,61 @@ def step_impl(context):
 @then(u'the first byte should be 0x00')
 def step_impl(context):
     for src, dst in context.pairs:
-        assert dst[0] == 0
+        assert ord(dst[0]) == 0
     context.prefex_hex_count = 2
 
-@then(u'the remain bits with be same of the original hex sequence')
+@then(u'the remain bits will be same of the original hex sequence')
 def step_impl(context):
-    assert False
+    for src, dst in context.pairs:
+        assert context.prefex_hex_count in [1, 2]
+        assert len(src) == len(dst) - context.prefex_hex_count
+
+        dst_hexes = []
+        for byte in dst:
+            dst_hex.extend([ord(byte) & 0xF0, ord(byte) & 0x0F])
+
+        for src_hex, dst_hex in zip(src, dst_hexes[context.prefex_hex_count:]):
+            assert src_hex == dst_hex
 
 @then(u'decode the compactly encoded hex sequence will get the original one')
 def step_impl(context):
-    assert False
+    import trie
+    for src, dst in context.pairs:
+        decoded_hexes = trie.bin_to_hexarraykey(dst)
+        for src_hex, dst_hex in zip(src, decoded_hexes):
+            assert src_hex == dst_hex
 
 @given(u'an odd length hex sequence')
 def step_impl(context):
-    assert False
+    context.srcs = [
+        [0x0],
+        [0x1, 0x2, 0x3]
+    ]
 
 @then(u'the first byte should start with 0x1')
 def step_impl(context):
-    assert False
+    for src, dst in context.pairs:
+        assert ord(dst[0]) & 0xF0 == 0x10
+    context.prefex_hex_count = 1
 
 @when(u'append a terminator')
 def step_impl(context):
-    assert False
+    for src in context.srcs:
+        src.append(16)
+
+@when(u'remove terminator from source')
+def step_impl(context):
+    for src in context.srcs:
+        del src[-1]
 
 @then(u'the first byte should start with 0x2')
 def step_impl(context):
-    assert False
+    for src, dst in context.pairs:
+        assert ord(dst[0]) & 0xF0 == 0x20
+    context.prefex_hex_count = 1
 
 @then(u'the first byte should start with 0x3')
 def step_impl(context):
-    assert False
+    for src, dst in context.pairs:
+        assert ord(dst[0]) & 0xF0 == 0x30
+    context.prefex_hex_count = 1
