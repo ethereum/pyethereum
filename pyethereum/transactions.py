612dbc8fc832f039e44202981e3099ba43414392
Heiko Heiko <heiko@heiko.org>
2014-03-19 15:04:09 +0100

    cleanup pep8 and imports

--- pyethereum/transactions.py
+++ pyethereum/transactions.py
@@ -1,8 +1,11 @@
-from pybitcointools import *
-import rlp
 import re
+import rlp
+from pybitcointools import sha256, bin_sha256, encode_pubkey, privtopub
+from pybitcointools import ecdsa_raw_sign, ecdsa_raw_recover
 
-class Transaction():
+
+class Transaction(object):
+
     def __init__(*args):
         self = args[0]
         if len(args) == 2:
@@ -14,8 +17,8 @@ class Transaction():
             self.fee = args[4]
             self.data = args[5]
 
-    def parse(self,data):
-        if re.match('^[0-9a-fA-F]*$',data):
+    def parse(self, data):
+        if re.match('^[0-9a-fA-F]*$', data):
             data = data.decode('hex')
         o = rlp.decode(data)
         self.nonce = o[0]
@@ -26,19 +29,29 @@ class Transaction():
         self.v = o[5]
         self.r = o[6]
         self.s = o[7]
-        rawhash = sha256(rlp.encode([self.nonce,self.to,self.value,self.fee,self.data]))
-        pub = encode_pubkey(ecdsa_raw_recover(rawhash,(self.v,self.r,self.s)),'bin')
+        rawhash = sha256(
+            rlp.encode([self.nonce, self.to, self.value, self.fee, self.data]))
+        pub = encode_pubkey(
+            ecdsa_raw_recover(rawhash, (self.v, self.r, self.s)), 'bin')
         self.sender = bin_sha256(pub[1:])[-20:]
         return self
 
-    def sign(self,key):
-        rawhash = sha256(rlp.encode([self.nonce,self.to,self.value,self.fee,self.data]))
-        self.v,self.r,self.s = ecdsa_raw_sign(rawhash,key)
+    def sign(self, key):
+        rawhash = sha256(
+            rlp.encode([self.nonce, self.to, self.value, self.fee, self.data]))
+        self.v, self.r, self.s = ecdsa_raw_sign(rawhash, key)
         self.sender = bin_sha256(privtopub(key)[1:])[-20:]
         return self
 
     def serialize(self):
-        return rlp.encode([self.nonce, self.to, self.value, self.fee, self.data, self.v, self.r, self.s])
+        return rlp.encode([self.nonce,
+                           self.to,
+                           self.value,
+                           self.fee,
+                           self.data,
+                           self.v,
+                           self.r,
+                           self.s])
 
     def hex_serialize(self):
         return self.serialize().encode('hex')
