030c3919c0f978e5ab81352f3407195dfdae3f2c
Chen Houwu <chenhouwu@gmail.com>
2014-03-26 23:42:37 +0800

    remove inadvertently re-commited the code expected to be removed

--- pyethereum/__init__.py
+++ pyethereum/__init__.py
@@ -1,16 +1,1 @@
-import logging
-import logging.config
-from ConfigParser import ParsingError
-import sys
 
-
-if not 'logging' in sys.modules:  # Defensively avoid to override external logging-configurations (e.g. when imported as a library).
-    try:
-        logging.config.fileConfig('logging.conf')
-    except ParsingError as e:
-        print "no usable logging.conf (Error: \"%s\") -> will log to stdout" % e
-        logging.basicConfig(format='[%(asctime)s] %(name)s %(levelname)s %(message)s', level=logging.DEBUG)
-
-logger = logging.getLogger(__name__)
-
-logger.debug('logging set up')
