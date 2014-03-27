586f07cf6654d24b86f43a1940e544e3c9ad75a9
Chen Houwu <chenhouwu@gmail.com>
2014-03-28 01:05:12 +0800

    add: auto discover hooks for tags

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,3 @@
-from trie_hook import trie_feature_hooker, trie_scenario_hooker
-
 # USE: BEHAVE_DEBUG_ON_ERROR=yes     (to enable debug-on-error)
 from distutils.util import strtobool as _bool
 import os
@@ -15,16 +13,49 @@ def after_step(context, step):
         ipdb.post_mortem(step.exc_traceback)
 
 
+def auto_discover_hooks():
+    from os import path
+    from importlib import import_module
+
+    hooks = {}
+    hook_dir = path.join(path.dirname(__file__), 'hooks')
+    for f in os.listdir(hook_dir):
+        if not f.endswith('.py'):
+            continue
+        name = f[:-3]
+        module = import_module('hooks.{0}'.format(name))
+        if not hasattr(module, 'hook'):
+            continue
+        hooks[name] = getattr(module, 'hook')
+
+    return hooks
+
+hooks = auto_discover_hooks()
+
+
 def before_feature(context, feature):
-    if 'trie' in feature.tags:
-        trie_feature_hooker.before(context, feature)
+    for tag in feature.tags:
+        if tag in hooks:
+            if hasattr(hooks[tag], 'before_feature'):
+                hooks[tag].before_feature(context, feature)
 
 
 def after_feature(context, feature):
-    if 'trie' in feature.tags:
-        trie_feature_hooker.after(context, feature)
+    for tag in feature.tags:
+        if tag in hooks:
+            if hasattr(hooks[tag], 'after_feature'):
+                hooks[tag].after_feature(context, feature)
 
 
 def before_scenario(context, scenario):
-    if 'trie' in scenario.feature.tags:
-        trie_scenario_hooker.before(context, scenario)
+    for tag in scenario.tags:
+        if tag in hooks:
+            if hasattr(hooks[tag], 'before_scenario'):
+                hooks[tag].before_scenario(context, scenario)
+
+
+def after_scenario(context, scenario):
+    for tag in scenario.tags:
+        if tag in hooks:
+            if hasattr(hooks[tag], 'after_scenario'):
+                hooks[tag].after_scenario(context, scenario)
--- features/hooks/trie.py
+++ features/hooks/trie.py
@@ -2,40 +2,37 @@ import os
 from os import path
 
 
-class TrieFeatureHooker(object):
+class TrieHook(object):
     db_dir = "tmp"
     db_file_name = "trie-test.db"
 
     def __init__(self):
         self.db_path = path.join(self.db_dir, self.db_file_name)
 
-    def before(self, context, feature):
+    def before_feature(self, context, feature):
         from pyethereum import trie
-        self.__create_dir()
-        self.__delete_db()
+        self._create_dir()
+        self._delete_db()
 
         context.trie = trie.Trie(self.db_path)
-        self.__load_fixture()
+        self._load_fixture()
 
-    def after(self, context, feature):
+    def after_feature(self, context, feature):
         del context.trie
-        self.__delete_db()
+        self._delete_db()
 
-    def __create_dir(self):
+    def _create_dir(self):
         if not path.exists(self.db_dir):
             os.mkdir(self.db_dir)
 
-    def __delete_db(self):
+    def _delete_db(self):
         import leveldb
         leveldb.DestroyDB(self.db_path)
 
-    def __load_fixture(self):
+    def _load_fixture(self):
         pass
 
-
-class TrieScenarioHooker(object):
-
-    def before(self, context, scenario):
+    def before_scenario(self, context, scenario):
 
         if 'load_data' in scenario.tags:
             context.execute_steps(u'''
@@ -46,9 +43,7 @@ class TrieScenarioHooker(object):
                         "Z", "0", "Z0", "0Z"])
             )
 
-    def after(self, context, scenario):
+    def after_scenario(self, context, scenario):
         pass
 
-
-trie_feature_hooker = TrieFeatureHooker()
-trie_scenario_hooker = TrieScenarioHooker()
+hook = TrieHook()
