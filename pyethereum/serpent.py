ad015d4fff42b140f88afde5c69f1aea8a0e8e00
Konrad Feldmeier <konrad.feldmeier@brainbot.com>
2014-04-10 15:08:15 +0200

    Add entry-point in tools for serpent
    This removes the `main()` method from the library part of `pyethereum/serpent.py` and
    moves it into `tools/serpent_cli.py`. It also adds a console_script entry in
    `setup.py`, so after installation, the serpent-CLI tool becomes available as `serpent`.
    
    (If you're using buildout, this will simply be `bin/serpent`).
    
    This is part of #59

--- pyethereum/serpent.py
+++ pyethereum/serpent.py
@@ -433,31 +433,3 @@ def decode_datalist(arr):
     for i in range(0, len(arr), 32):
         o.append(frombytes(arr[i:i + 32]))
     return o
-
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print "serpent <command> <arg1> <arg2> ..."
-    else:
-        cmd = sys.argv[2] if sys.argv[1][0] == '-' else sys.argv[1]
-        if sys.argv[1] == '-s':
-            args = re.findall(r'\S\S*', sys.stdin.read()) + sys.argv[3:]
-        elif sys.argv[1] == '-B':
-            args = [sys.stdin.read()] + sys.argv[3:]
-        elif sys.argv[1] == '-b':
-            args = [sys.stdin.read()[:-1]] + sys.argv[3:]  # remove trailing \n
-        elif sys.argv[1] == '-j':
-            args = [json.loads(sys.stdin.read())] + sys.argv[3:]
-        elif sys.argv[1] == '-J':
-            args = json.loads(sys.stdin.read()) + sys.argv[3:]
-        elif sys.argv[1] == '-h':
-            args = [x.decode('hex') for x in sys.argv[3:]]
-        else:
-            cmd = sys.argv[1]
-            args = sys.argv[2:]
-        if args[0] in os.listdir(os.getcwd()):
-            args[0] = open(args[0]).read()
-        o = vars()[cmd](*args)
-        if isinstance(o, (list, dict)):
-            print json.dumps(o)
-        else:
-            print o.encode('hex')
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 
 from setuptools import setup, find_packages
 
-console_scripts = ['eth=pyethereum.eth:main']
+console_scripts = ['eth=pyethereum.eth:main',
+                   'serpent=tools.serpent_cli:main']
 
 setup(name="pyethereum",
       packages=find_packages("."),
