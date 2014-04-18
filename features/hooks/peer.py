ecff03b1be6de6eae76f45747cdfb21c709ed55a
Chen Houwu <chenhouwu@gmail.com>
2014-04-18 18:45:28 +0800

    extract common test utils out to a separate file

--- features/hooks/peer.py
+++ features/hooks/peer.py
@@ -1,70 +1,20 @@
 import mock
+import utils
 
 
 class PeerHook(object):
     def before_feature(self, context, feature):
         from pyethereum.packeter import packeter
-        import socket
 
         context.packeter = packeter
         packeter.configure(context.conf)
-        context._connection = mock.MagicMock(spec=socket.socket)
-
-    def mock_peer(self, context):
-        from pyethereum.peer import Peer
-        context.peer = Peer(context._connection, '127.0.0.1', 1234)
-        peer = context.peer
-
-        def side_effect():
-            for i in range(3):
-                peer.loop_body()
-
-        peer.run = mock.MagicMock()
-        peer.run.side_effect = side_effect
-
-    def mock_connection_recv(self, context):
-        received_packets = []
-
-        def add_recv_packet(packet):
-            received_packets.append(packet)
-
-            def genarator(bufsize):
-                for packet in received_packets:
-                    for i in range(0, len(packet), bufsize):
-                        yield packet[i: i + bufsize]
-                    yield ''
-
-            status = dict()
-
-            def side_effect(bufsize):
-                if 'genarator' not in status:
-                    status.update(genarator=genarator(bufsize))
-
-                try:
-                    return status['genarator'].next()
-                except:
-                    return ''
-
-            context._connection.recv.side_effect = side_effect
-
-        context._connection.recv = mock.MagicMock()
-        context.add_recv_packet = add_recv_packet
-        context.add_recv_packet('')
-        context.received_packets = received_packets
-
-    def mock_connection_send(self, context):
-        context.sent_packets = []
-
-        def side_effect(packet):
-            context.sent_packets.append(packet)
-            return len(packet)
-
-        context._connection.send = mock.MagicMock(side_effect=side_effect)
+        context._connection = utils.mock_connection()
 
     def before_scenario(self, context, scenario):
-        self.mock_peer(context)
-        self.mock_connection_recv(context)
-        self.mock_connection_send(context)
+        context.peer = utils.mock_peer(context._connection, '127.0.0.1', 1234)
+        context.add_recv_packet, context.received_packets = \
+            utils.mock_connection_recv(context._connection)
+        context.sent_packets = utils.mock_connection_send(context._connection)
 
         time_sleep_patcher = mock.patch('time.sleep')
         time_sleep_patcher.start()
---
+++ features/hooks/utils.py
@@ -0,0 +1,62 @@
+import mock
+
+
+def mock_peer(connection, ip, port):
+    from pyethereum.peer import Peer
+    peer = Peer(connection, ip, port)
+
+    def side_effect():
+        for i in range(3):
+            peer.loop_body()
+
+    peer.run = mock.MagicMock()
+    peer.run.side_effect = side_effect
+    peer.start = peer.run
+    return peer
+
+
+def mock_connection():
+    import socket
+    return mock.MagicMock(spec=socket.socket)
+
+
+def mock_connection_recv(connection):
+    received_packets = []
+
+    def add_recv_packet(packet):
+        received_packets.append(packet)
+
+        def genarator(bufsize):
+            for packet in received_packets:
+                for i in range(0, len(packet), bufsize):
+                    yield packet[i: i + bufsize]
+                yield ''
+
+        status = dict()
+
+        def side_effect(bufsize):
+            if 'genarator' not in status:
+                status.update(genarator=genarator(bufsize))
+
+            try:
+                return status['genarator'].next()
+            except:
+                return ''
+
+        connection.recv.side_effect = side_effect
+
+    connection.recv = mock.MagicMock()
+    add_recv_packet('')
+
+    return (add_recv_packet, received_packets)
+
+
+def mock_connection_send(connection):
+    sent_packets = []
+
+    def side_effect(packet):
+        sent_packets.append(packet)
+        return len(packet)
+
+    connection.send = mock.MagicMock(side_effect=side_effect)
+    return sent_packets
