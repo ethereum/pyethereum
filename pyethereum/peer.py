f5cfa0c1c89a6e785e6832839368a7dbe48a602c
Heiko Heiko <heiko@heiko.org>
2014-04-20 18:57:39 +0200

    removed debug prints

--- pyethereum/peer.py
+++ pyethereum/peer.py
@@ -94,9 +94,7 @@ class Peer(StoppableLoopThread):
         '''
         while True:
             try:
-                print 'before recv...'
                 self.recv_buffer += self.connection().recv(2048)
-                print 'after recv...'
             except socket.error:
                 break
         length = len(self.recv_buffer)
