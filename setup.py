250854f01c849927b2035f1c176800dfedf57da9
Vitalik Buterin <v@buterin.com>
2014-04-20 11:16:48 -0400

    Version upgrade

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ console_scripts = ['eth=pyethereum.eth:main',
                    'pyethtool=tools.pyethtool_cli:main']
 
 setup(name="pyethereum",
+      version='0.0.1',
       packages=find_packages("."),
       install_requires=['six', 'leveldb', 'bitcoin', 'pysha3'],
       entry_points=dict(console_scripts=console_scripts))
