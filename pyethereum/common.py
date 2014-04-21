05133639bad0c96f9791d7c5f14b0ead7800afee
Heiko Heiko <heiko@heiko.org>
2014-04-21 08:43:29 +0200

    report instances instead class names; asser implementation of loop_body

--- pyethereum/common.py
+++ pyethereum/common.py
@@ -5,29 +5,27 @@ logger = logging.getLogger(__name__)
 
 
 class StoppableLoopThread(threading.Thread):
+
     def __init__(self):
         super(StoppableLoopThread, self).__init__()
         self._stopped = False
         self.lock = threading.Lock()
 
-    def thread_name(self):
-        return self.__class__
-
     def stop(self):
         with self.lock:
             self._stopped = True
         logger.debug(
-            'Thread {0} is requested to stop'.format(self.thread_name()))
+            'Thread {0} is requested to stop'.format(self))
 
     def stopped(self):
         with self.lock:
             return self._stopped
 
     def run(self):
-        logger.debug('Thread {0} start to run'.format(self.thread_name()))
+        logger.debug('Thread {0} start to run'.format(self))
         while not self.stopped():
             self.loop_body()
-        logger.debug('Thread {0} stopped'.format(self.thread_name()))
+        logger.debug('Thread {0} stopped'.format(self))
 
     def loop_body(self):
-        pass
+        raise Exception('Not Implemented')
