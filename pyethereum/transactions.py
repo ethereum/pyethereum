5edbfdb22b4df33cc0e1b3a760e5c7f46edeb1e6
Chen Houwu <chenhouwu@gmail.com>
2014-04-24 00:42:20 +0800

    pep8

--- pyethereum/transactions.py
+++ pyethereum/transactions.py
@@ -2,25 +2,30 @@ import re
 import rlp
 from bitcoin import encode_pubkey
 from bitcoin import ecdsa_raw_sign, ecdsa_raw_recover
-from opcodes import reverse_opcodes
 from utils import big_endian_to_int as decode_int
 from utils import int_to_big_endian as encode_int
 from utils import sha3, privtoaddr
 import utils
 import sys
 
+
 class Transaction(object):
 
     """
     A transaction is stored as:
     [ nonce, value, gasprice, startgas, to, data, v, r, s]
-    nonce is the number of transactions already sent by that account,
-    encoded in binary form (eg. 0 -> '', 7 -> '\x07', 1000 -> '\x03\xd8').
-    (v,r,s) is the raw Electrum-style signature of the transaction without the signature
-    made with the private key corresponding to the sending account, with 0 <= v <= 3.
-    From an Electrum-style signature (65 bytes) it is possible to extract the public key,
-    and thereby the address, directly. A valid transaction is one where
-    (i) the signature is well-formed (ie. 0 <= v <= 3, 0 <= r < P, 0 <= s < N, 0 <= r < P - N if v >= 2), and
+
+    nonce is the number of transactions already sent by that account, encoded
+    in binary form (eg.  0 -> '', 7 -> '\x07', 1000 -> '\x03\xd8').
+
+    (v,r,s) is the raw Electrum-style signature of the transaction without the
+    signature made with the private key corresponding to the sending account,
+    with 0 <= v <= 3. From an Electrum-style signature (65 bytes) it is
+    possible to extract the public key, and thereby the address, directly.
+
+    A valid transaction is one where:
+    (i) the signature is well-formed (ie. 0 <= v <= 3, 0 <= r < P, 0 <= s < N,
+        0 <= r < P - N if v >= 2), and
     (ii) the sending account has enough funds to pay the fee and the value.
     """
 
@@ -42,25 +47,27 @@ class Transaction(object):
                 if self.r > 0 and self.s > 0:
                     rawhash = sha3(rlp.encode(self.serialize(False)))
                     pub = encode_pubkey(
-                        ecdsa_raw_recover(rawhash, (self.v, self.r, self.s)), 'bin')
+                        ecdsa_raw_recover(rawhash, (self.v, self.r, self.s)),
+                        'bin')
                     self.sender = sha3(pub[1:])[-20:].encode('hex')
             # does not include signature
             else:
-                self.v, self.r, self.s = 0,0,0
+                self.v, self.r, self.s = 0, 0, 0
                 self.sender = 0
 
     # nonce,value,gasprice,startgas,code
     @classmethod
     def contract(*args):
-        sys.stderr.write("Deprecated method. Use pyethereum.transactions.contract "+
-                         "instead of pyethereum.transactions.Transaction.contract\n")
+        sys.stderr.write(
+            "Deprecated method. Use pyethereum.transactions.contract " +
+            "instead of pyethereum.transactions.Transaction.contract\n")
         return contract(*args[1:])
 
     @classmethod
     def parse(cls, data):
         if re.match('^[0-9a-fA-F]*$', data):
             data = data.decode('hex')
-        o = rlp.decode(data) + ['','','']
+        o = rlp.decode(data) + ['', '', '']
         tx = cls(decode_int(o[0]),
                  decode_int(o[1]),
                  decode_int(o[2]),
@@ -81,7 +88,6 @@ class Transaction(object):
     def coerce_to_hex(self, n):
         return n.encode('hex') if len(n) == 20 else n
 
-
     def serialize(self, signed=True):
         return rlp.encode([encode_int(self.nonce),
                            encode_int(self.value),
@@ -99,7 +105,8 @@ class Transaction(object):
     def hash(self):
         return sha3(self.serialize())
 
-def contract(nonce, endowment, gasprice, startgas, code, v=0,r=0, s=0):
+
+def contract(nonce, endowment, gasprice, startgas, code, v=0, r=0, s=0):
     tx = Transaction(nonce, endowment, gasprice, startgas, '', code)
-    tx.v, tx.r, tx.s = v,r,s
+    tx.v, tx.r, tx.s = v, r, s
     return tx
