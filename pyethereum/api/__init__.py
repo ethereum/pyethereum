9fbbb20e44321ff69ed38ad77ce25adcf22b8167
Chen Houwu <chenhouwu@gmail.com>
2014-04-24 16:57:49 +0800

    auto discover apis

--- pyethereum/api/__init__.py
+++ pyethereum/api/__init__.py
@@ -1,7 +1,13 @@
-from common import app
+from common import app  # noqa
 
 
 def auto_discover():
-    return app
+    from os import path
+    import pkgutil
+    current_dir = path.dirname(__file__)
+    for finder, name, ispkg in pkgutil.iter_modules(path=[current_dir]):
+        main_name = path.splitext(name)[0]
+        if main_name.endswith('api'):
+            finder.find_module(name).load_module(name)
 
 auto_discover()
