cf5474b894a221df8d41e522633f60a22d8faee0
Chen Houwu <chenhouwu@gmail.com>
2014-04-26 19:18:27 +0800

    add waitress server for multithread support

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(name="pyethereum",
       version='0.0.1',
       packages=find_packages("."),
       install_requires=[
-          'six', 'leveldb', 'bitcoin', 'pysha3', 'netifaces', 'bottle', 'hyp'],
+          'six', 'leveldb', 'bitcoin', 'pysha3', 'netifaces',
+          'bottle', 'waitress', 'hyp'],
       entry_points=dict(console_scripts=console_scripts))
