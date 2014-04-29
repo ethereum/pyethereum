7663f49a9f072aa2107a71e2b1113f395ad3b928
Chen Houwu <chenhouwu@gmail.com>
2014-04-29 12:25:27 +0800

    add: cors support

--- pyethereum/apiserver.py
+++ pyethereum/apiserver.py
@@ -1,7 +1,7 @@
 import logging
 import threading
 import time
-from bottle import run as bottle_run
+from bottle import run as bottle_run, response
 from bottle import Bottle
 from dispatch import receiver
 from marshmallow import Serializer, fields
@@ -31,7 +31,6 @@ class ApiServer(threading.Thread):
                    host=self.listen_host, port=self.port)
 
 
-
 def response_async_data(name, make_response, req=None):
     state = dict(res=None, ready=False)
 
@@ -47,10 +46,10 @@ def response_async_data(name, make_response, req=None):
     return Exception()
 
 
-
-####### create server ######
+# ###### create server ######
 
 api_server = ApiServer()
+
 
 @receiver(config_ready)
 def config_api_server(sender, **kwargs):
@@ -59,7 +58,20 @@ def config_api_server(sender, **kwargs):
 app = Bottle()
 app.config['autojson'] = True
 
-############# Blocks ######################
+
+@app.hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = \
+        'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = \
+        'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+# ############ Blocks ######################
 
 
 class Binary(fields.Raw):
@@ -87,7 +99,8 @@ class BlocksResponder(Responder):
 @app.get(base_url + '/blocks/')
 def blocks():
     logger.debug('blocks/')
-    return BlocksResponder().respond(chain_manager.get_chain(start=0, count=20))
+    return BlocksResponder().respond(
+        chain_manager.get_chain(start=0, count=20))
 
 
 @app.get(base_url + '/blocks/<blockhash>')
@@ -100,27 +113,30 @@ def block(blockhash=None):
         return '404 Not Found'  # 404
 
 
-######### Peers ###################
+# ######## Peers ###################
 
 class PeerSerializer(Serializer):
     ip = fields.Function(lambda o: o['ip'])
     port = fields.Function(lambda o: str(o['port']))
     node_id = fields.Function(lambda o: o['node_id'].encode('hex'))
-    
+
+
 class PeerResponder(Responder):
     TYPE = 'peer'
     SERIALIZER = PeerSerializer
 
+
 def make_peers_response(peers):
-    peers = [dict(ip=ip, port=port, node_id=node_id) for (ip, port, node_id) in peers]
+    peers = [dict(ip=ip, port=port, node_id=node_id) for
+             (ip, port, node_id) in peers]
     return PeerResponder().respond(peers)
+
 
 @app.get(base_url + '/connected_peers/')
 def connected_peers():
     return response_async_data('connected_peers', make_peers_response)
 
 
-@app.get(base_url +'/known_peers/')
+@app.get(base_url + '/known_peers/')
 def known_peers():
     return response_async_data('known_peers', make_peers_response)
-
