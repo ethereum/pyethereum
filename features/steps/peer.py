e9a1721de25ca882da4a447db6a4675a3338853d
Heiko Heiko <heiko@heiko.org>
2014-05-05 17:08:00 +0200

    del: test w/ invalid assumptions

--- features/steps/peer.py
+++ features/steps/peer.py
@@ -423,12 +423,6 @@ def step_impl(context):
     context.peer.send_Blocks = instrument(context.peer.send_Blocks)
 
 
-@then(u'peer.send_Blocks should be called once with the blocks data')  # noqa
-def step_impl(context):
-    assert context.peer.send_Blocks.call_count == 1
-    assert context.peer.send_Blocks.call_args[0][0] == context.blocks_data
-
-
 @when(u'peer.send_NotInChain is called')  # noqa
 def step_impl(context):
     context.peer.send_NotInChain('some hash')
