c9bcb09bf2c48ad48474c63a312069cd45b9203c
Heiko Heiko <heiko@heiko.org>
2014-05-08 11:37:38 +0200

    currency contract from test.py

--- tests/test_contracts.py
+++ tests/test_contracts.py
@@ -43,3 +43,45 @@ else:
     tx4 = t.Transaction(3, gasprice, startgas, addr, 0, serpent.encode_datalist(['harry', 60])).sign(k)
     s, o = pb.apply_tx(blk, tx4)
     assert serpent.decode_datalist(o) == [1]
+
+def test_currency():
+    k, v, k2, v2 = accounts()    
+    scode2 = '''
+if !contract.storage[1000]:
+    contract.storage[1000] = 1
+    contract.storage[0x%s] = 1000
+    return(1)
+elif msg.datasize == 1:
+    addr = msg.data[0]
+    return(contract.storage[addr])
+else:
+    from = msg.sender
+    fromvalue = contract.storage[from]
+    to = msg.data[0]
+    value = msg.data[1]
+    if fromvalue >= value:
+        contract.storage[from] = fromvalue - value
+        contract.storage[to] = contract.storage[to] + value
+        return(1)
+    else:
+        return(0)
+    ''' % v
+    code2 = serpent.compile(scode2)
+    blk = b.genesis({v: 10**18})
+    tx4 = t.contract(0, gasprice, startgas, 0, code2).sign(k)
+    s, addr = pb.apply_tx(blk, tx4)
+    tx5 = t.Transaction(1, gasprice, startgas, addr, 0, '').sign(k)
+    s, o = pb.apply_tx(blk, tx5)
+    assert serpent.decode_datalist(o) == [1]
+    tx6 = t.Transaction(2, gasprice, startgas, addr, 0, serpent.encode_datalist([v2, 200])).sign(k)
+    s, o = pb.apply_tx(blk, tx6)
+    assert serpent.decode_datalist(o) == [1]
+    tx7 = t.Transaction(3, gasprice, startgas, addr, 0, serpent.encode_datalist([v2, 900])).sign(k)
+    s, o = pb.apply_tx(blk, tx7)
+    assert serpent.decode_datalist(o) == [0]
+    tx8 = t.Transaction(4, gasprice, startgas, addr, 0, serpent.encode_datalist([v])).sign(k)
+    s, o = pb.apply_tx(blk, tx8)
+    assert serpent.decode_datalist(o) == [800]
+    tx9 = t.Transaction(5, gasprice, startgas, addr, 0, serpent.encode_datalist([v2])).sign(k)
+    s, o = pb.apply_tx(blk, tx9)
+    assert serpent.decode_datalist(o) == [200]
