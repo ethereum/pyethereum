465374696b5b47e87daf38570d2e2c0bacd51fb7
Heiko Heiko <heiko@heiko.org>
2014-05-11 17:29:46 +0200

    add: num_transactions

--- pyethereum/indexdb.py
+++ pyethereum/indexdb.py
@@ -32,3 +32,11 @@ class AccountTxIndex(object):
         for key in self.db.db.RangeIter(include_value=False, key_from=account_from):
             if key.endswith(zero):
                 yield key[:-4]
+
+    def num_transactions(self, account, start=0):
+        if not self.db.has_key(self._key(account, start)):
+            return start
+        test = start + 1
+        while self.db.has_key(self._key(account, test*2)):
+            test *= 2
+        return self.num_transactions(account, test)
