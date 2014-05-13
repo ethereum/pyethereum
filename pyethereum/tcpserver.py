af3a8a8cca6b2c9c433155f01146b3385f4c7507
Chen Houwu <chenhouwu@gmail.com>
2014-05-13 21:39:40 +0800

    enable upnp when startup

--- pyethereum/tcpserver.py
+++ pyethereum/tcpserver.py
@@ -12,6 +12,71 @@ import signals
 logger = logging.getLogger(__name__)
 
 
+def upnp_add(port):
+    '''
+    :param port: local port
+    :return: `None` if failed, `external_ip, external_port` if succeed
+    '''
+    logger.debug('Setting UPNP')
+
+    import miniupnpc
+    upnpc = miniupnpc.UPnP()
+    upnpc.discoverdelay = 200
+    ndevices = upnpc.discover()
+    logger.debug('%d UPNP device(s) detected', ndevices)
+
+    if not ndevices:
+        return None
+
+    upnpc.selectigd()
+    external_ip = upnpc.externalipaddress()
+    logger.debug('external ip: %s', external_ip)
+    logger.debug('status: %s, connection type: %s',
+                 upnpc.statusinfo(),
+                 upnpc.connectiontype())
+
+    # find a free port for the redirection
+    external_port = port
+    found = False
+
+    while True:
+        redirect = upnpc.getspecificportmapping(external_port, 'TCP')
+        if redirect is None:
+            found = True
+            break
+        if external_port >= 65535:
+            break
+        external_port = external_port + 1
+
+    if not found:
+        logger.debug('No redirect candidate %s TCP => %s port %u TCP',
+                     external_ip, upnpc.lanaddr, port)
+        return None
+
+    logger.debug('trying to redirect %s port %u TCP => %s port %u TCP',
+                 external_ip, external_port, upnpc.lanaddr, port)
+
+    res = upnpc.addportmapping(external_port, 'TCP',
+                               upnpc.lanaddr, port,
+                               'pyethereum p2p port %u' % external_port,
+                               '')
+
+    if res:
+        logger.info('Success to redirect %s port %u TCP => %s port %u TCP',
+                    external_ip, external_port, upnpc.lanaddr, port)
+    else:
+        return None
+    return upnpc, external_ip, external_port
+
+
+def upnp_delete(upnpc, external_port):
+    res = upnpc.deleteportmapping(external_port, 'TCP')
+    if res:
+        logger.debug('Successfully deleted port mapping')
+    else:
+        logger.debug('Failed to remove port mapping')
+
+
 class TcpServer(StoppableLoopThread):
 
     def __init__(self):
@@ -21,6 +86,10 @@ class TcpServer(StoppableLoopThread):
         self.sock = None
         self.ip = '0.0.0.0'
         self.port = 31033
+
+        self.upnpc = None
+        self.external_ip = None
+        self.external_port = None
 
     def configure(self, config):
         self.listen_host = config.get('network', 'listen_host')
@@ -35,6 +104,15 @@ class TcpServer(StoppableLoopThread):
         self.sock = sock
         self.ip, self.port = sock.getsockname()
         logger.info("TCP server started {0}:{1}".format(self.ip, self.port))
+
+        # setup upnp
+        try:
+            upnp_res = upnp_add(self.port)
+            if upnp_res:
+                self.upnpc, self.external_ip, self.external_port = upnp_res
+        except Exception as e:
+            logger.debug('upnp failed: %s', e)
+
         super(TcpServer, self).pre_loop()
 
     def loop_body(self):
@@ -49,6 +127,12 @@ class TcpServer(StoppableLoopThread):
                                               connection=connection,
                                               ip=ip,
                                               port=port)
+
+    def post_loop(self):
+        if self.upnpc:
+            upnp_delete(self.upnpc, self.external_port)
+        super(TcpServer, self).post_loop()
+
 
 tcp_server = TcpServer()
 
