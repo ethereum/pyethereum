43293099957d999403602282ece33d63cc21e79b
Chen Houwu <chenhouwu@gmail.com>
2014-05-21 11:48:53 +0800

    close #100

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -126,7 +126,7 @@ def test_genesis_db():
     assert blk == blk2
     assert blk != blk3
 
-@pytest.mark.wip
+
 def test_trie_state_root_nodep():
     def int_to_big_endian(integer):
         if integer == 0:
@@ -136,12 +136,17 @@ def test_trie_state_root_nodep():
             s = '0' + s
         return s.decode('hex')
     CPP_PoC5_GENESIS_STATE_ROOT_HEX_HASH = \
-        '2f4399b08efe68945c1cf90ffe85bbe3ce978959da753f9e649f034015b8817d'
+        '12582945fc5ad12c3e7b67c4fc37a68fc0d52d995bb7f7291ff41a2739a7ca16'
     GENESIS_INITIAL_ALLOC = \
-        {"8a40bfaa73256b60764c1bf40675a99083efb075": 2 ** 200,
-         "e6716f9544a56c530d868e4bfbacb172315bdead": 2 ** 200,
-         "1e12515ce3e0f817a4ddef9ca55788a1d66bd2df": 2 ** 200,
-         "1a26338f0d905e295fccb71fa9ea849ffa12aaf4": 2 ** 200}
+        {"8a40bfaa73256b60764c1bf40675a99083efb075": 2 ** 200,  # (G)
+         "e6716f9544a56c530d868e4bfbacb172315bdead": 2 ** 200,  # (J)
+         "1e12515ce3e0f817a4ddef9ca55788a1d66bd2df": 2 ** 200,  # (V)
+         "1a26338f0d905e295fccb71fa9ea849ffa12aaf4": 2 ** 200,  # (A)
+         "2ef47100e0787b915105fd5e3f4ff6752079d5cb": 2 ** 200,  # (M)
+         "cd2a3d9f938e13cd947ec05abc7fe734df8dd826": 2 ** 200,  # (R)
+         "6c386a4b26f73c802f34673f7248bb118f97424a": 2 ** 200,  # (HH)
+         "e4157b34ea9615cfbde6b4fda419828124b70c78": 2 ** 200,  # (CH)
+         }
     EMPTYSHA3 = utils.sha3('')
     assert EMPTYSHA3.encode('hex') == \
         'c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470'
