4d29e3bda05ccab344381cebb7c25d15213c4f1b
Heiko Heiko <heiko@heiko.org>
2014-05-28 16:54:07 +0200

    refactored to generic index

--- pyethereum/indexdb.py
+++ pyethereum/indexdb.py
@@ -3,43 +3,74 @@ import db
 from utils import get_index_path
 
 
-class AccountTxIndex(object):
-    "acct|nonce > tx"
+class Index(object):
 
-    def __init__(self):
+    """
+    stores a list of values for a key
+    datastructure: namespace|key|valnum > val
+    """
+
+    def __init__(self, namespace):
+        self.namespace = namespace
         self.db = db.DB(get_index_path())
 
-    def _key(self, account, nonce):
-        return account + struct.pack('>I', nonce)
+    def _key(self, key, valnum=None):
+        return self.namespace + key + struct.pack('>I', valnum)
 
-    def add_transaction(self, account, nonce, transaction_hash):
-        self.db.put(self._key(account, nonce), transaction_hash)
+    def add(self, key, valnum, value):
+        assert isinstance(value, str)
+        self.db.put(self._key(key, valnum), value)
 
-    def get_transactions(self, account, offset=0):
+    def get(self, key, offset=0):
         assert not self.db.uncommitted
-        account_from = self._key(account, offset)
+        key_from = self._key(key, offset)
         for k, v in self.db.db.RangeIter(include_value=True,
-                                         key_from=account_from):
-            if k.startswith(account) and struct.unpack('>I', k[-4:]) >= offset:
+                                         key_from=key_from):
+            if k.startswith(self.namespace + key) and struct.unpack('>I', k[-4:]) >= offset:
                 yield v
 
-    def delete_transactions(self, account, offset=0):
-        while self._key(account, offset) in self.db:
-            self.db.delete(self._key(account, offset))
+    def delete(self, key, offset=0):
+        while self._key(key, offset) in self.db:
+            self.db.delete(self._key(key, offset))
             offset += 1
 
-    def get_accounts(self, account_from=None):
+    def keys(self, key_from=None):
         assert not self.db.uncommitted
+        if key_from is not None:
+            key_from = self.namespace + key_from
         zero = struct.pack('>I', 0)
         for key in self.db.db.RangeIter(include_value=False,
-                                        key_from=account_from):
+                                        key_from=key_from):
             if key.endswith(zero):
-                yield key[:-4]
+                yield key[len(self.namespace):-4]
 
-    def num_transactions(self, account, start=0):
-        if self._key(account, start) not in self.db:
+    def num_values(self, key, start=0):
+        if self._key(key, start) not in self.db:
             return start
         test = start + 1
-        while self._key(account, test*2) in self.db:
+        while self._key(key, test * 2) in self.db:
             test *= 2
-        return self.num_transactions(account, test)
+        return self.num_values(key, test)
+
+
+class AccountTxIndex(Index):
+
+    "acct|txnonce > tx"
+
+    def __init__(self):
+        super(AccountTxIndex, self).__init__('tx')
+
+    def add_transaction(self, account, nonce, transaction_hash):
+        self.add(account, nonce, transaction_hash)
+
+    def get_transactions(self, account, offset=0):
+        return self.get(account, offset)
+
+    def delete_transactions(self, account, offset=0):
+        self.delete(account, offset)
+
+    def get_accounts(self, account_from=None):
+        return self.keys(key_from=account_from)
+
+    def num_transactions(self, account, start=0):
+        return self.num_values(account)
