79ea3bc4caef68ff46b1cb3ba1b83adc868c218c
Heiko Heiko <heiko@heiko.org>
2014-07-21 13:00:37 +0200

    add: test for simple CPP value transfer tx

--- tests/test_remoteblocks.py
+++ tests/test_remoteblocks.py
@@ -1,12 +1,15 @@
 from pyethereum import blocks
 from pyethereum import rlp
 from test_chain import set_db, get_chainmanager
-from remoteblocksdata import data as hex_rlp_encoded_data
+from remoteblocksdata import data_poc5v23_1, data_poc5v23_2
 import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 
-def test_import_chain():
+
+
+
+def do_test(hex_rlp_encoded_data):
 	set_db()
 	chain_manager = get_chainmanager()
 	data = rlp.decode(hex_rlp_encoded_data.decode('hex'))
@@ -14,3 +17,11 @@ def test_import_chain():
 	chain_manager.receive_chain(transient_blocks)
 
 
+
+def test_import_local_cpp_chain_blk_5_tx():
+	# simple transaction
+	do_test(data_poc5v23_2)
+
+def test_import_remote_chain_blk_11_contract():
+	# contract creation
+	do_test(data_poc5v23_1)
