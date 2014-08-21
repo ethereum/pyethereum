0712b2379ad197f8f8ff99de1bcefa97a59eb67a
Heiko Heiko <heiko@heiko.org>
2014-08-21 15:57:32 +0200

    fix for eebee9df4538fb24fee5062872b9c1e8cc85194f

--- pyethereum/dispatch/dispatcher.py
+++ pyethereum/dispatch/dispatcher.py
@@ -161,7 +161,7 @@ class Signal(object):
 
         with self.lock:
             self._clear_dead_receivers()
-            for index in self.receivers:
+            for index in range(len(self.receivers)):
                 (r_key, _) = self.receivers[index]
                 if r_key == lookup_key:
                     del self.receivers[index]
