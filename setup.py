20c2d1ffa07b706a73db5a6672a3a50f1b60a75b
Vitalik Buterin <v@buterin.com>
2014-08-24 18:36:49 -0400

    version

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-
 from setuptools import setup, find_packages
 
-console_scripts = [ 'pyeth=pyethereum.eth:main',
-                    'pyethclient=pyethereum.ethclient:main']
+console_scripts = ['pyeth=pyethereum.eth:main',
+                   'pyethclient=pyethereum.ethclient:main']
 
 setup(name="pyethereum",
-      version='0.2.5',
+      version='0.6.0',
       packages=find_packages("."),
       install_requires=[
           'six', 'leveldb', 'bitcoin', 'pysha3',
-          'miniupnpc','ethereum-serpent', 'pytest',
+          'miniupnpc', 'ethereum-serpent', 'pytest',
           'bottle', 'waitress', 'docopt'],
       entry_points=dict(console_scripts=console_scripts))
