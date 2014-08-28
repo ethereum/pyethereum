35e5e37fc60df92ce7ca78fd6d6481403fd0416d
Heiko Heiko <heiko@heiko.org>
2014-08-28 11:29:16 +0200

    fix: ignor nrewly mined blocks

--- pyethereum/blockfetcherpatch.py
+++ pyethereum/blockfetcherpatch.py
@@ -8,13 +8,14 @@ configure to connect to one peer, no mine
 
 """
 print('IMPORTED BLOCKFETCHERPATCH')
-fn = 'blocks.0-2k.poc6.p27.hexdata'
+fn = 'blocks.1-1137.poc6.p28.hexdata'
 
 
-NUM_BLOCKS_PER_REQUEST = 20
+NUM_BLOCKS_PER_REQUEST = 200
 
 ##############
 import sys
+from operator import attrgetter
 from pyethereum.packeter import packeter
 import pyethereum.chainmanager as chainmanager
 import pyethereum.utils as utils
@@ -30,25 +31,36 @@ fh = open(fn,'w')
 peer.Peer.blk_counter = 0
 peer.Peer.blk_requested = set()
 
+collected_blocks = []
+peer.Peer.lowest_block = None
+
 def _recv_Blocks(self, data):
-    print("RECEIVED BLOCKS", len(data)) # youngest to oldest
-    for x in reversed(data):
+    print("RECEIVED BLOCKS", len(data)) # youngest (highest blk) to oldest (lowest blk)
+    assert blocks.TransientBlock(rlp.encode(data[0])).number >= blocks.TransientBlock(rlp.encode(data[-1])).number
+    for x in data:
         enc = rlp.encode(x)
         tb = blocks.TransientBlock(enc)
         print tb
         self.blk_counter += 1
-        fh.write(enc.encode('hex') + '\n') # LOG line
-        h = utils.sha3(enc)
-        print('received block %s %d' % (h.encode('hex'), self.blk_counter))
-
-    if len(data) == NUM_BLOCKS_PER_REQUEST:
-        block_hash = utils.sha3(rlp.encode(data[-1]))
-        print("ASKING FOR MORE HASHES", block_hash.encode('hex'))
-        print("genesis is", blocks.genesis().hex_hash())
-        self.send_GetBlockHashes(block_hash, NUM_BLOCKS_PER_REQUEST)
-    else:
-        print 'done'
-        sys.exit(0)
+        if self.lowest_block is None:
+            self.lowest_block = tb.number
+        else:
+            if self.lowest_block - 1 == tb.number:
+                self.lowest_block = tb.number
+            else: # i.e. newly mined block sent
+                return
+        if tb not in collected_blocks:
+            collected_blocks.append(tb)
+        # exit if we are at the genesis
+        if tb.number == 1:
+            print 'done'
+            for tb in sorted(collected_blocks, key=attrgetter('number')):
+                print 'writing', tb
+                fh.write(tb.rlpdata.encode('hex') + '\n') # LOG line
+            sys.exit(0)
+    # fetch more
+    print("ASKING FOR MORE HASHES", tb.hash.encode('hex'), tb.number)
+    self.send_GetBlockHashes(tb.hash, NUM_BLOCKS_PER_REQUEST)
 
 peer.Peer._recv_Blocks = _recv_Blocks
 
@@ -60,12 +72,15 @@ def _recv_Hello(self, data):
     head_hash = data[7]
     print "head_hash", head_hash.encode('hex')
     #self.send_GetBlocks([head_hash])
+    assert not len(collected_blocks)
     self.send_GetBlockHashes(head_hash, NUM_BLOCKS_PER_REQUEST)
+
+
 peer.Peer._recv_Hello = _recv_Hello
 
 def _recv_BlockHashes(self, data):
     print("RECEIVED BLOCKHASHES", len(data)) # youngest to oldest
-    print [x.encode('hex') for x in data]
+    #print [x.encode('hex') for x in data]
     block_hashes = data # youngest to oldest
     self.send_GetBlocks(block_hashes)
 
