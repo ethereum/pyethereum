b7b0f34e7e88d35a65f1dced1ddc2b01b9a03bc4
Heiko Heiko <heiko@heiko.org>
2014-08-30 15:09:08 +0200

    mv miner, synchronizer to separate files

---
+++ pyethereum/miner.py
@@ -0,0 +1,97 @@
+import struct
+import logging
+import blocks
+import processblock
+import utils
+
+logger = logging.getLogger(__name__)
+
+class Miner():
+    """
+    Mines on the current head
+    Stores received transactions
+
+    The process of finalising a block involves four stages:
+    1) Validate (or, if mining, determine) uncles;
+    2) validate (or, if mining, determine) transactions;
+    3) apply rewards;
+    4) verify (or, if mining, compute a valid) state and nonce.
+    """
+
+    def __init__(self, parent, uncles, coinbase):
+        self.nonce = 0
+        self.block = blocks.Block.init_from_parent(
+            parent, coinbase, uncles=[u.list_header() for u in uncles])
+        self.pre_finalize_state_root = self.block.state_root
+        self.block.finalize()
+        logger.debug('Mining #%d %s', self.block.number, self.block.hex_hash())
+        logger.debug('Difficulty %s', self.block.difficulty)
+
+
+    def add_transaction(self, transaction):
+        old_state_root = self.block.state_root
+        # revert finalization
+        self.block.state_root = self.pre_finalize_state_root
+        try:
+            success, output = processblock.apply_transaction(
+                self.block, transaction)
+        except processblock.InvalidTransaction as e:
+            # if unsuccessfull the prerequistes were not fullfilled
+            # and the tx isinvalid, state must not have changed
+            logger.debug('Invalid Transaction %r: %r', transaction, e)
+            success = False
+
+        # finalize
+        self.pre_finalize_state_root = self.block.state_root
+        self.block.finalize()
+
+        if not success:
+            logger.debug('transaction %r not applied', transaction)
+            assert old_state_root == self.block.state_root
+            return False
+        else:
+            assert transaction in self.block.get_transactions()
+            logger.debug(
+                'transaction %r applied to %r res: %r',
+                transaction, self.block, output)
+            assert old_state_root != self.block.state_root
+            return True
+
+
+
+    def get_transactions(self):
+        return self.block.get_transactions()
+
+    def mine(self, steps=1000):
+        """
+        It is formally defined as PoW: PoW(H, n) = BE(SHA3(SHA3(RLP(Hn)) o n))
+        where:
+        RLP(Hn) is the RLP encoding of the block header H, not including the
+            final nonce component;
+        SHA3 is the SHA3 hash function accepting an arbitrary length series of
+            bytes and evaluating to a series of 32 bytes (i.e. 256-bit);
+        n is the nonce, a series of 32 bytes;
+        o is the series concatenation operator;
+        BE(X) evaluates to the value equal to X when interpreted as a
+            big-endian-encoded integer.
+        """
+
+        nonce_bin_prefix = '\x00' * (32 - len(struct.pack('>q', 0)))
+        target = 2 ** 256 / self.block.difficulty
+        rlp_Hn = self.block.serialize_header_without_nonce()
+
+        for nonce in range(self.nonce, self.nonce + steps):
+            nonce_bin = nonce_bin_prefix + struct.pack('>q', nonce)
+            # BE(SHA3(SHA3(RLP(Hn)) o n))
+            h = utils.sha3(utils.sha3(rlp_Hn) + nonce_bin)
+            l256 = utils.big_endian_to_int(h)
+            if l256 < target:
+                self.block.nonce = nonce_bin
+                assert self.block.check_proof_of_work(self.block.nonce) is True
+                assert self.block.get_parent()
+                logger.debug(
+                    'Nonce found %d %r', nonce, self.block)
+                return self.block
+
+        self.nonce = nonce
+        return False
---
+++ pyethereum/synchronizer.py
@@ -0,0 +1,140 @@
+
+from operator import attrgetter
+import logging
+import blocks
+logger = logging.getLogger(__name__)
+
+class HashChainTask(object):
+    """
+    - get hashes chain until we see a known block hash
+    """
+
+    NUM_HASHES_PER_REQUEST = 200
+    GENESIS_HASH = blocks.genesis().hash
+
+    def __init__(self, chain_manager, peer, block_hash):
+        self.chain_manager = chain_manager
+        self.peer = peer
+        self.hash_chain = [] # [youngest, ..., oldest]
+        self.request(block_hash)
+
+    def request(self, block_hash):
+        self.peer.send_GetBlockHashes(block_hash, self.NUM_HASHES_PER_REQUEST)
+
+    def received_block_hashes(self, block_hashes):
+        if self.GENESIS_HASH in block_hashes:
+            logger.warn('%r has different chain starting from genesis', self.peer)
+        for bh in block_hashes:
+            if bh in self.chain_manager or bh == self.GENESIS_HASH:
+                logger.debug('%r matching block hash found %r', self.peer, bh.encode('hex'))
+                return list(reversed(self.hash_chain))
+            self.hash_chain.append(bh)
+        self.request(bh)
+
+
+class SynchronizationTask(object):
+    """
+    Created if we receive a unknown block w/o known parent. Possibly from a different branch.
+
+    - get hashes chain until we see a known block hash
+    - request missing blocks
+
+    - once synced
+        - rerequest blocks that lacked a reference before syncing
+    """
+    NUM_BLOCKS_PER_REQUEST = 200
+
+    def __init__(self, chain_manager, peer, block_hash):
+        self.chain_manager = chain_manager
+        self.peer = peer
+        self.hash_chain = [] # [oldest to youngest]
+        logger.debug('%r syncing %r', self.peer, block_hash.encode('hex'))
+        self.hash_chain_task = HashChainTask(self.chain_manager, self.peer, block_hash)
+
+    def received_block_hashes(self, block_hashes):
+        res = self.hash_chain_task.received_block_hashes(block_hashes)
+        if res:
+            self.hash_chain = res
+            logger.debug('%r hash chain with %d hashes for missing blocks', self.peer, len(self.hash_chain))
+            self.request_blocks()
+
+    def received_blocks(self, transient_blocks):
+        logger.debug('%r received %d of %d missing blocks', self.peer, len(transient_blocks), len(self.hash_chain))
+        for tb in transient_blocks:
+            if len(self.hash_chain) and self.hash_chain[0] == tb.hash:
+                self.hash_chain.pop(0)
+            else:
+                logger.debug('%r received unexpected block %r', self.peer, tb)
+                return False
+        if self.hash_chain:
+            # still blocks to fetch
+            logger.debug('%r still missing %d blocks', self.peer, len(self.hash_chain))
+            self.request_blocks()
+        else: # done
+            return True
+
+    def request_blocks(self):
+        logger.debug('%r requesting %d of %d missing blocks', self.peer, self.NUM_BLOCKS_PER_REQUEST, len(self.hash_chain))
+        self.peer.send_GetBlocks(self.hash_chain[:self.NUM_BLOCKS_PER_REQUEST])
+
+
+class Synchronizer(object):
+    """"
+    Cases:
+        on "recv_Hello": received unknown head_hash w/ sufficient difficulty
+        on "recv_Blocks": received block w/o parent (new block mined, competing chain discovered)
+
+    Naive Strategy:
+        assert we see a block for which we have no parent
+        assume that the sending peer knows the parent
+        if we have not yet syncer for this unknown block:
+            create new syncer
+            sync direction genesis until we see known block_hash
+            sync also (re)requests the block we missed, so it can be added on top of the synced chain
+        else
+            do nothing
+            syncing (if finished) will be started with the next broadcasted block w/ missing parent
+    """
+
+    def __init__(self, chain_manager):
+        self.chain_manager = chain_manager
+        self.synchronization_tasks = {} # peer > syncer # syncer.unknown_hash as marker for task
+
+    def synchronize_unknown_block(self, peer, block_hash):
+        "Case: block with unknown parent. Fetches unknown ancestors and this block"
+        logger.debug('%r sync %r', peer, block_hash)
+        assert block_hash not in self.chain_manager
+        if peer and not peer in self.synchronization_tasks:
+            logger.debug('%r new sync task', peer)
+            self.synchronization_tasks[peer] = SynchronizationTask(self.chain_manager, peer, block_hash)
+        else:
+            logger.debug('%r already has a synctask, sorry', peer)
+
+
+    def synchronize_hello(self, peer, block_hash, total_difficulty):
+        "Case: unknown head with sufficient difficulty"
+        logger.debug('%r Hello  with %r %d', peer,  block_hash.encode('hex'), total_difficulty)
+        assert block_hash not in self.chain_manager
+        # guesstimate the max difficulty difference possible for a sucessfully competing chain
+        # worst case if skip it: we are on a stale chain until the other catched up
+        # assume difficulty is constant
+        num_blocks_behind = 7
+        avg_uncles_per_block = 4
+        max_diff = self.chain_manager.head.difficulty * num_blocks_behind * (1 + avg_uncles_per_block)
+        if total_difficulty + max_diff > self.chain_manager.head.difficulty:
+            logger.debug('%r sufficient difficulty, syncing', peer)
+            self.synchronize_unknown_block(peer, block_hash)
+        else:
+            logger.debug('%r insufficient difficulty, not syncing', peer)
+
+    def received_block_hashes(self, peer, block_hashes):
+        if peer in self.synchronization_tasks:
+            logger.debug("Synchronizer.received_block_hashes for: %r", peer)
+            self.synchronization_tasks[peer].received_block_hashes(block_hashes)
+
+    def received_blocks(self, peer, transient_blocks):
+        if peer in self.synchronization_tasks:
+            res = self.synchronization_tasks[peer].received_blocks(transient_blocks)
+            if res is True:
+                logger.debug("Synchronizer.received_blocks: chain w %r synced", peer)
+                del self.synchronization_tasks[peer]
