57fb69720eff5f8550669219e13db7c541a694f0
Heiko Heiko <heiko@heiko.org>
2014-12-16 22:21:37 +0100

    new dbs in tests

--- tests/utils.py
+++ tests/utils.py
@@ -12,19 +12,24 @@ import json
 import os
 import tempfile
 import pyethereum.utils as utils
-
-
+from pyethereum.db import DB as DB
+from pyethereum.config import get_default_config as _get_default_config
 __TESTDATADIR = "../tests"
 
 tempdir = tempfile.mktemp()
 
+def new_config():
+    cfg = _get_default_config()
+    cfg.set('misc', 'data_dir', tempfile.mktemp())
 
 def load_test_data(fname):
     return json.loads(open(os.path.join(__TESTDATADIR, fname)).read())
 
+def new_db():
+    return DB(utils.db_path(tempfile.mktemp()))
 
-def set_db(name=''):
-    if name:
-        utils.data_dir.set(os.path.join(tempdir, name))
-    else:
-        utils.data_dir.set(tempfile.mktemp())
+# def set_db(name=''):
+#     if name:
+#         utils.data_dir.set(os.path.join(tempdir, name))
+#     else:
+#         utils.data_dir.set(tempfile.mktemp())
