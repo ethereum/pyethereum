52bc7ddfe95bff036aba6b0c486f15b1e5dac125
Heiko Heiko <heiko@heiko.org>
2014-12-18 21:22:05 +0100

    rm: behave

--- features/hooks/packeter.py
+++
@@ -1,7 +0,0 @@
-class PacketerHook(object):
-    def before_feature(self, context, feature):
-        from pyethereum.packeter import Packeter
-        context.packeter = packeter = Packeter()
-        packeter.configure(context.conf)
-
-hook = PacketerHook()
