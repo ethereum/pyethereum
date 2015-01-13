b805fb56604e6b34356beccbeb6f448d00fcc364
Vitalik Buterin <v@buterin.com>
2015-01-13 13:43:58 -0500

    changed tests

--- tests/test_vm.py
+++ tests/test_vm.py
@@ -17,6 +17,7 @@ logger = get_logger()
 # hint: use 'py.test' with the '-s' option to dump logs to the console
 configure_logging(':trace')
 
+
 def check_testdata(data_keys, expected_keys):
     assert set(data_keys) == set(expected_keys), \
         "test data changed, please adjust tests"
@@ -30,14 +31,26 @@ def vm_tests_fixtures():
     """
     # FIXME: assert that repo is uptodate
     # cd fixtures; git pull origin develop; cd ..;  git commit fixtures
-    filenames = os.listdir(os.path.join('fixtures', 'VMTests'))
-    files = [os.path.join('fixtures', 'VMTests', f) for f in filenames]
+
+    # Recursively traverse directories to get list of files
+    dirs = [os.path.join('fixtures', 'VMTests')]
+    files = []
+    i = 0
+    while i < len(dirs):
+        children = [os.path.join(dirs[i], f) for f in os.listdir(dirs[i])]
+        for f in children:
+            if os.path.isdir(f):
+                dirs.append(f)
+            else:
+                files.append(f)
+        i += 1
     vm_fixtures = {}
     try:
-        for f, fn in zip(files, filenames):
+        for f in files:
+            fn = os.path.split(f)[1]
             if f[-5:] == '.json':
                 vm_fixtures[fn[:-5]] = json.load(open(f, 'r'))
-    except IOError, e:
+    except IOError:
         raise IOError("Could not read vmtests.json from fixtures",
                       "Make sure you did 'git submodule init'")
     return vm_fixtures
