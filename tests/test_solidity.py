1e7f7e32b3887b2caf7d6433c181957180ba98a5
Vitalik Buterin <v@buterin.com>
2015-02-09 18:35:48 -0500

    Updated testcase

--- tests/test_solidity.py
+++ tests/test_solidity.py
@@ -2,22 +2,24 @@ import pytest
 from pyethereum import tester, utils, abi
 
 serpent_contract = """
-extern solidity: [sub:_:i]
+extern solidity: [sub2:_:i]
 
 def main(a):
-    return(a.sub() * 2)
+    return(a.sub2() * 2)
 
-def sub():
+def sub1():
     return(5)
 
 """
 
 solidity_contract = """
+contract serpent { function sub1() returns (int256 y) {} }
+
 contract foo {
     function main(address a) returns (int256 y) {
-        y = a.sub() * 2;
+        y = serpent(a).sub1() * 2;
     }
-    function sub() returns (int256 y) {
+    function sub2() returns (int256 y) {
         y = 7;
     }
 }
@@ -29,7 +31,8 @@ def test_interop():
     s = tester.state()
     c1 = s.abi_contract(serpent_contract)
     c2 = s.abi_contract(solidity_contract, language='solidity')
-    assert c1.sub() == 5
-    assert c2.sub() == 7
+    assert c1.sub1() == 5
+    assert c2.sub2() == 7
     assert c1.main(c2.address) == 14
+    print c2.main(c1.address, output='raw').encode('hex')
     assert c2.main(c1.address) == 10
