8e7946c75966eb46af9ef0d554c37b7db66b2e09
Vitalik Buterin <v@buterin.com>
2015-05-25 18:52:41 -0400

    Added getaddress support in keystorer.py script

--- tools/keystorer.py
+++ tools/keystorer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 
 import sys, json, os
+from bitcoin import privtopub
 try:
     import keys
 except:
@@ -9,9 +10,16 @@ except:
     except:
         raise Exception("keys module not found")
 
+
+def privtoaddr(x):
+    if len(x) > 32:
+        x = keys.decode_hex(x)
+    return keys.sha3(privtopub(x)[1:])[12:]
+
+
 # Help
 if len(sys.argv) < 2:
-    print("Use `keystorer.py create <pw> <optional privkey>` to create a key store file, and `keystorer.py decode <filename> <pw>` to decode a key store file")
+    print("Use `keystorer.py create <pw> <optional privkey>` to create a key store file, and `keystorer.py getprivkey <filename> <pw>` or `keystorer.py getaddress <filename> <pw> to get a privkey/address from a key store file, respectively")
 # Create a json
 elif sys.argv[1] == 'create':
     if len(sys.argv) < 4:
@@ -25,7 +33,7 @@ elif sys.argv[1] == 'create':
     open(j["id"]+'.json', 'w').write(json.dumps(j, indent=4))
     print("Wallet creation successful, file saved at: " + j["id"] + ".json")
 # Decode a json
-elif sys.argv[1] == 'decode':
+elif sys.argv[1] in ('getprivkey', 'getaddress'):
     if len(sys.argv) < 3:
         raise Exception("Need filename")
     json = json.loads(open(sys.argv[2]).read())
@@ -33,4 +41,8 @@ elif sys.argv[1] == 'decode':
         raise Exception("Need password")
     pw = sys.argv[3]
     print("Applying hard key derivation function. Wait a little")
-    print(keys.encode_hex(keys.decode_keystore_json(json, pw)))
+    k = keys.decode_keystore_json(json, pw)
+    if sys.argv[1] == 'getprivkey':
+        print(keys.encode_hex(k))
+    else:
+        print(keys.encode_hex(privtoaddr(k)))
