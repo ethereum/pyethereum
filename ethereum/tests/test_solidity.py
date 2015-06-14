d3de282409d953153868ca72198540b54666696b
Ryan Casey <ryepdx@gmail.com>
2015-06-14 16:11:44 -0700

    Add support for Solidity constructors.

--- ethereum/tests/test_solidity.py
+++ ethereum/tests/test_solidity.py
@@ -1,4 +1,13 @@
+from rlp.utils import encode_hex
 from ethereum import tester
+
+def needs_solidity(function):
+    def decorated(*args, **kwargs):
+        if 'solidity' in tester.languages:
+            return function(*args, **kwargs)
+
+    return decorated
+
 serpent_contract = """
 extern solidity: [sub2:_:i]
 
@@ -23,10 +32,8 @@ contract zoo {
 }
 """
 
-
+@needs_solidity
 def test_interop():
-    if 'solidity' not in tester.languages:
-        return
     s = tester.state()
     c1 = s.abi_contract(serpent_contract)
     c2 = s.abi_contract(solidity_contract, language='solidity')  # should be zoo
@@ -34,3 +41,31 @@ def test_interop():
     assert c2.sub2() == 7
     assert c1.main(c2.address) == 14
     assert c2.main(c1.address) == 10
+
+constructor_contract = """
+contract gondor {
+    address public ruler;
+
+    function gondor(address steward) {
+        if (steward == 0x0) {
+            ruler = msg.sender;
+        } else {
+            ruler = steward;
+        }
+    }
+}
+"""
+
+@needs_solidity
+def test_constructor():
+    s = tester.state()
+    c1 = s.abi_contract(
+        constructor_contract, language='solidity',
+        contract_name='gondor'
+    )
+    c2 = s.abi_contract(
+        constructor_contract, constructor_args=[tester.a1],
+        language='solidity', contract_name='gondor'
+    )
+    assert c1.ruler() != c2.ruler()
+    assert c2.ruler() == encode_hex(tester.a1)
