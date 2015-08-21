1c184f41f58ca3a05a1eca53834e431197addbfa
Heiko Heiko <heiko@heiko.org>
2015-08-21 10:22:09 +0200

    version bump

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(name="ethereum",
       install_requires=install_requires,
       tests_require=tests_require,
       entry_points=dict(console_scripts=console_scripts),
-      version='0.9.73',
+      version='0.9.74',
       cmdclass=cmdclass
       )
