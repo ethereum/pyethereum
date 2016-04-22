c2207bdd3715515363ae5acb943a3186682a16cd
Piper Merriam <pipermerriam@gmail.com>
2016-04-22 11:26:17 -0600

    dirty

--- ethereum/guardian/cli.py
+++ ethereum/guardian/cli.py
@@ -3,6 +3,8 @@ import time
 import json
 import random
 import logging
+import shutil
+import gevent
 
 import click
 
@@ -22,6 +24,7 @@ from devp2p.utils import (
 from ethereum.utils import (
     big_endian_to_int,
     encode_int,
+    encode_int32,
     sha3,
 )
 from ethereum.db import LevelDB
@@ -72,6 +75,10 @@ def get_chaindata_dir(data_dir):
     return os.path.join(data_dir, 'chaindata')
 
 
+def get_stateroot_file_path(data_dir):
+    return os.path.join(data_dir, 'stateroot')
+
+
 def get_private_key_path(data_dir):
     return os.path.join(data_dir, 'private_key')
 
@@ -80,7 +87,7 @@ def get_genesis_file_path(data_dir):
     return os.path.join(data_dir, 'genesis.json')
 
 
-@click.group()
+@click.group(chain=True)
 @click.option(
     '--data-dir',
     help="Directory that the blockchain database will be stored",
@@ -156,14 +163,30 @@ def cli(ctx, data_dir, genesis, private_key, config, log_level):
     '--seed',
     help="The seed value that will be used to generate the private key",
 )
+@click.option(
+    '--gen-genesis/--no-gen-genesis',
+    default=False,
+    help="Should the `genesis.json` file be generated.",
+)
+@click.option(
+    '--write-genesis-state/--no-write-genesis-state',
+    default=False,
+    help="Should the initial genesis state be written to the chain directory.",
+)
+@click.option(
+    '--prompt/--no-prompt',
+    default=True,
+    help="The file path to write the genesis file to",
+)
 @click.pass_context
-def init(ctx, seed):
+def init(ctx, seed, gen_genesis, write_genesis_state, prompt):
     configuration = ctx.obj
 
     data_dir = configuration['data_dir']
 
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
+        click.echo("Created data_dir at: `{0}`".format(data_dir))
 
     if seed is None:
         seed = encode_int(random.randint(1e32, 1e64))
@@ -172,23 +195,77 @@ def init(ctx, seed):
 
     if not os.path.exists(private_key_file_path):
         private_key = sha3(seed)
+        configuration['private_key'] = private_key
         with open(private_key_file_path, 'w') as private_key_file:
             private_key_file.write(private_key)
+        click.echo("Generated private key at: `{0}`".format(private_key_file_path))
 
     genesis_path = configuration['genesis']
-    if not os.path.exists(genesis_path):
-        raise click.ClickException("No genesis file found.  Cannot write initial chain data")
+    if gen_genesis or not os.path.exists(genesis_path):
+        if gen_genesis or not prompt or click.confirm("No genesis file found.  Would you like to generate one?"):
+            ctx.invoke(generate_genesis, prompt=prompt)
+        else:
+            click.exit(0)
 
     chaindata_dir = get_chaindata_dir(data_dir)
 
+    chaindata_present = os.path.exists(chaindata_dir)
+
+    if chaindata_present and write_genesis_state:
+        ctx.invoke(reset, prompt=prompt)
+    elif chaindata_present and not write_genesis_state:
+        ctx.exit(0)
+
     if not os.path.exists(chaindata_dir):
         os.makedirs(chaindata_dir)
+        click.echo("Created chaindata dir at: `{0}`".format(chaindata_dir))
 
     with open(genesis_path) as genesis_file:
         genesis_config = json.load(genesis_file)
 
     db = LevelDB(chaindata_dir)
-    initialize_genesis_state(genesis_config, db)
+    genesis_state = initialize_genesis_state(genesis_config, db)
+
+    stateroot = genesis_state.root
+    stateroot_file_path = get_stateroot_file_path(data_dir)
+    with open(stateroot_file_path, 'w') as stateroot_file:
+        stateroot_file.write(stateroot)
+
+    # need to release the lock on the leveldb
+    del genesis_state
+    del db.db
+    del db
+
+    click.echo("Blockchain database initialized with genesis stateroot: `{0}`".format(stateroot.encode('hex')))
+
+
+@cli.command()
+@click.option(
+    '--prompt/--no-prompt',
+    default=True,
+    help="The file path to write the genesis file to",
+)
+@click.pass_context
+def reset(ctx, prompt):
+    configuration = ctx.obj
+
+    data_dir = configuration['data_dir']
+    chaindata_dir = get_chaindata_dir(data_dir)
+    stateroot_file_path = get_stateroot_file_path(data_dir)
+
+    if prompt:
+        click.confirm("Are you sure you want to wipe the chain data from: `{0}`".format(chaindata_dir), abort=True)
+
+    try:
+        shutil.rmtree(chaindata_dir)
+        click.echo("Erased chaindata dir at: `{0}`".format(chaindata_dir))
+    except OSError:
+        pass
+
+    try:
+        os.remove(stateroot_file_path)
+    except OSError:
+        pass
 
 
 @cli.command()
@@ -196,8 +273,13 @@ def init(ctx, seed):
     '--outfile',
     help="The file path to write the genesis file to",
 )
+@click.option(
+    '--prompt/--no-prompt',
+    default=True,
+    help="The file path to write the genesis file to",
+)
 @click.pass_context
-def generate_genesis(ctx, outfile):
+def generate_genesis(ctx, outfile, prompt):
     configuration = ctx.obj
 
     if 'private_key' not in configuration:
@@ -207,12 +289,14 @@ def generate_genesis(ctx, outfile):
 
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
+        click.echo("Created data_dir at: `{0}`".format(data_dir))
 
     if outfile is None:
         outfile = get_genesis_file_path(data_dir)
 
     if os.path.exists(outfile):
-        click.confirm("You are about to overwrite the genesis file at `{0}`.  Are you sure?".format(outfile), abort=True)
+        if prompt:
+            click.confirm("You are about to overwrite the genesis file at `{0}`.  Are you sure?".format(outfile), abort=True)
 
     genesis_data = {
         'timestamp': int(time.time()),
@@ -223,7 +307,7 @@ def generate_genesis(ctx, outfile):
     with open(outfile, 'w') as f:
         f.write(json.dumps(genesis_data))
 
-    click.echo("Wrote new genesis file to `{0}`".format(outfile))
+    click.echo("Wrote new genesis file at: `{0}`".format(outfile))
 
 
 def initialize_genesis_state(genesis_config, db):
@@ -303,37 +387,34 @@ def initialize_genesis_state(genesis_config, db):
         # validation code
         vcode2 = call_method(genesis, CASPER, casper_ct, 'getGuardianValidationCode', [index])
         assert vcode2 == vcode
-    GENESIS_TIMESTAMP = genesis_config['timestamp']
 
+    genesis_timestamp = genesis_config['timestamp']
 
     # Set the starting RNG seed to equal to the number of casper guardians
     # in genesis
     genesis.set_storage(RNGSEEDS, encode_int32(2**256 - 1), genesis.get_storage(CASPER, 0))
     # Set the genesis timestamp
-    genesis.set_storage(GENESIS_TIME, encode_int32(0), genesis_config['timestamp'])
+    genesis.set_storage(GENESIS_TIME, encode_int32(0), genesis_timestamp)
 
-    logger.info('Genesis time: %s', GENESIS_TIMESTAMP)
+    logger.info('Genesis time: %s', genesis_timestamp)
 
     return genesis
 
-def get_network(bet, bootstrap_nodes=None):
+
+DEFAULT_PORT = 29870
+DEFAULT_IP_ADDRESS = b'0.0.0.0'
+
+
+def get_network(bet, min_peers=1, max_peers=25, bootstrap_nodes=None,
+                ip_address=None, port=None):
     if bootstrap_nodes is None:
         bootstrap_nodes = []
-    # setup the bootstrap node (node0) enode
-    DEFAULT_PORT = 29870
-    DEFAULT_IP_ADDRESS = b'0.0.0.0'
 
-    PORT = get_arg('--port', int, DEFAULT_PORT)
-    IP_ADDRESS = get_arg('--ip-address', str, DEFAULT_IP_ADDRESS)
+    if ip_address is None:
+        ip_address = DEFAULT_IP_ADDRESS
 
-    BOOTSTRAP_NODE_IP = get_arg('--bootstrap-ip', str, DEFAULT_IP_ADDRESS)
-    BOOTSTRAP_NODE_PORT = get_arg('--bootstrap-port', int, DEFAULT_PORT)
-
-    bootstrap_node_privkey = mk_privkey(genesis_config['guardians'][0].decode('hex'))
-    bootstrap_node_pubkey = privtopub_raw(bootstrap_node_privkey)
-    bootstrap_enode = host_port_pubkey_to_uri(BOOTSTRAP_NODE_IP, BOOTSTRAP_NODE_PORT, bootstrap_node_pubkey)
-
-    print "Bootstrap ENODE: {0}".format(bootstrap_enode)
+    if port is None:
+        port = DEFAULT_PORT
 
     services = [NodeDiscovery, peermanager.PeerManager, GuardianService]
 
@@ -342,16 +423,16 @@ def get_network(bet, bootstrap_nodes=None):
     for s in services:
         update_config_with_defaults(config, s.default_config)
 
-    MIN_PEERS = get_arg('--min-peers', int, 2)
-    MAX_PEERS = get_arg('--max-peers', int, 25)
+    # TODO: can we just use the same private key as the guardian ethereum
+    # account?
+    private_key = sha3(bet.key)
+    config['node']['privkey_hex'] = private_key.encode('hex')
 
-    config['node']['privkey_hex'] = mk_privkey(bet.key).encode('hex')
+    config['p2p']['listen_port'] = port
+    config['p2p']['min_peers'] = min(10, min_peers)
+    config['p2p']['max_peers'] = max_peers
 
-    config['p2p']['listen_port'] = PORT
-    config['p2p']['min_peers'] = min(10, MIN_PEERS)
-    config['p2p']['max_peers'] = MAX_PEERS
-
-    config['discovery']['listen_port'] = PORT
+    config['discovery']['listen_port'] = port
     config['discovery']['bootstrap_nodes'] = bootstrap_nodes
 
     config['guardianservice']['agent'] = bet
@@ -366,14 +447,36 @@ def get_network(bet, bootstrap_nodes=None):
             service.register_with_app(app)
             assert hasattr(app.services, service.name)
 
-    import ipdb; ipdb.set_trace()
+    public_key = privtopub_raw(private_key)
+    enode = host_port_pubkey_to_uri(ip_address, port, public_key)
+    logger.info("Initializing Enode: `{0}`".format(enode))
 
     return app
 
 
 @cli.command()
+@click.option(
+    '--do-reset/--no-do-reset',
+    default=False,
+    help="Should the chain database be reset prior to run?",
+)
+@click.option(
+    '--auto-init/--no-auto-init',
+    default=True,
+    help="The seed value that will be used to generate the private key",
+)
+@click.option(
+    '--gen-genesis/--no-gen-genesis',
+    default=False,
+    help="The seed value that will be used to generate the private key",
+)
+@click.option(
+    '--prompt/--no-prompt',
+    default=True,
+    help="The file path to write the genesis file to",
+)
 @click.pass_context
-def run(ctx):
+def run(ctx, do_reset, auto_init, gen_genesis, prompt):
     """
     Run the guardian node.
     - (maybe) initialize the data-dir
@@ -384,18 +487,65 @@ def run(ctx):
     configuration = ctx.obj
 
     data_dir = configuration['data_dir']
-    private_key = configuration['private_key']
-    genesis_path = configuration['genesis']
     chaindata_dir = get_chaindata_dir(data_dir)
 
-    with open(genesis_path) as genesis_file:
-        genesis_config = json.load(genesis_file)
+    if do_reset:
+        ctx.invoke(reset, prompt=prompt)
+
+    if auto_init and not os.path.exists(chaindata_dir):
+        if auto_init or not prompt or click.confirm("It looks like this node has not been initialized.  Would you like to initialize?"):
+
+            ctx.invoke(init, gen_genesis=gen_genesis, prompt=prompt)
+            if gen_genesis:
+                gen_genesis = False
+
+    private_key = configuration['private_key']
+    genesis_path = configuration['genesis']
+    stateroot_file_path = get_stateroot_file_path(data_dir)
+
+    if gen_genesis or not os.path.exists(genesis_path):
+        if gen_genesis or not prompt or click.confirm("No genesis file found.  Would you like to generate one?"):
+            ctx.invoke(generate_genesis, prompt=prompt)
+        else:
+            raise click.ClickException("No genesis file found.  Cannot write initial chain data")
+
+    with open(stateroot_file_path) as stateroot_file:
+        stateroot = stateroot_file.read()
 
     db = LevelDB(chaindata_dir)
-    genesis_state = State('', db)
+    genesis_state = State(stateroot, db)
 
     bet = defaultBetStrategy(genesis_state, private_key)
-    bet.network = get_network()
+    network = get_network(bet)
+    bet.network = network
+
+    network.start()
+
+    logger.info("Entering Event Loop")
+    while True:
+        start = time.time()
+        try:
+            while start + 25 > time.time():
+                bet.tick()
+                gevent.sleep(random.random())
+        except KeyboardInterrupt:
+            logger.info("Stopping Guardian....")
+            interrupt_count = 0
+            while interrupt_count < 5:
+                try:
+                    network.stop()
+                    network.join()
+                    bet.db.commit()
+                    with open(stateroot_file_path, 'w') as stateroot_file:
+                        stateroot_file.write(bet.db.root)
+                    break
+                except KeyboardInterrupt:
+                    logger.warning("Shutting Down.  Press ctrl-c {0} more times to force stop".format(5 - interrupt_count))
+            else:
+                logger.error("Hard shutdown. stateroot=`{0}`".format(bet.db.root.encode('hex')))
+                ctx.exit(1)
+            logger.info("Shutdown complete: stateroot=`{0}`".format(bet.db.root.encode('hex')))
+            ctx.exit(0)
 
 
 if __name__ == '__main__':
