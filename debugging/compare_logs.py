a1d0418dbdede5916207a5b0a80e44588622e068
Konrad Feldmeier <konrad.feldmeier@brainbot.com>
2016-05-03 16:42:12 +0200

    Add stepping option for vm trace comparison

--- debugging/compare_logs.py
+++ debugging/compare_logs.py
@@ -3,17 +3,18 @@
 ./compare_logs.py
 
 Usage:
-    ./compare_logs.py -p <pyfile> [-b <blocknum>] [-g <gofile>]
+    ./compare_logs.py -p <pyfile> [-t <txnum>] [-g <gofile>]
+    ./compare_logs.py stepwise -p <pyfile> [-t <txnum>] [-g <gofile>]
 
 Help:
     -p <pyfile>     The filename for the pyethapp trace file.
-    -b <blocknum>   Which block to compare (default: 1).
+    -t <txnum>      Which tx to compare (default: 1).
     -g <gofile>     Optional: read this logfile for go trace.
 """
 import json
 from deepdiff import DeepDiff
 from pprint import pprint
-from print_pylog import read_pythons
+from print_pylog import read_pythons, read_transactions
 from docopt import docopt
 
 
@@ -23,17 +24,67 @@ def read_geth(fn):
     return slogs
 
 
+def print_stats(txs, num, name):
+    print "{}-trace contains [{}] transactions".format(name, len(txs))
+    print "{}-tx [{}] contains [{}] vm steps".format(name, num, len(txs[num]))
+
+
+get = lambda l, idx, default: l[idx] if len(l) > idx else default
+
 if __name__ == '__main__':
     options = docopt(__doc__)
-    print options
-    pyblocks = read_pythons(options['-p'])
-    assert len(pyblocks)
-    blocknum = int(options['-b'] or '1')
+    pytransactions = read_pythons(options['-p'])
+    txnum = int(options['-t'] or '1')
     if '-g' in options and options['-g'] is not None:
         gofile = options['-g']
     else:
-        gofile = "trace-{}.json".format(blocknum)
+        gofile = "trace-{}.json".format(txnum)
 
     goblock = read_geth(gofile)
-    pprint(DeepDiff(goblock, pyblocks[blocknum]))
-    print "diffed {} vs {}[{}]".format(gofile, options['-p'], blocknum)
+
+    gotransactions = read_transactions(goblock)
+
+    print_stats(gotransactions, txnum, "go")
+    print_stats(pytransactions, txnum, "py")
+
+    print "diffing tx [{}]".format(txnum)
+
+    go = py = None
+
+    deltastep = 0
+
+    def diff_(step, deltastep=0):
+        print "diffing vm step [{}]".format(i)
+        go = get(gotransactions[txnum], step, {})
+        py = get(pytransactions[txnum], step + deltastep, {})
+        return DeepDiff(go, py)
+
+    if options['stepwise']:
+        for i in range(max(len(gotransactions[txnum]), len(pytransactions[txnum]))):
+            diff = diff_(i, deltastep)
+            if len(diff):
+                goop = get(gotransactions[txnum], i, {}).get('op')
+                pyop = get(pytransactions[txnum], i, {}).get('op')
+                if goop != pyop:
+                    print "go: {} vs py: {}".format(goop, pyop)
+                    print "trying with delta"
+                    if not len(diff_(i, deltastep + 1)):
+                        print "delta +1"
+                        print "skipping py vm step[{}]; not in go:".format(i)
+                        print json.dumps(pytransactions[txnum][i + deltastep], indent=2, sort_keys=True)
+                        deltastep += 1
+                        continue
+                    elif not len(diff_(i, deltastep - 1)):
+                        print "delta -1"
+                        print "skipping go vm step[{}]; not in py:".format(i)
+                        print json.dumps(gotransactions[txnum][i + deltastep], indent=2, sort_keys=True)
+                        deltastep -= 1
+                        continue
+                    else:
+                        print "step [{}] differs:".format(i)
+                        pprint(diff)
+            else:
+                print "ok"
+    else:
+        pprint(DeepDiff(goblock, pytransactions[txnum]))
+        print "diffed {} vs {}[{}]".format(gofile, options['-p'], txnum)
--- debugging/print_pylog.py
+++ debugging/print_pylog.py
@@ -7,21 +7,29 @@ from collections import defaultdict
 def read_pythons(fn='pylog.jsons'):
     with open(fn) as f:
         lines = [json.loads(line) for line in f.readlines()]
-    blocks = defaultdict(list)
-    bnum = 0
+    txs = list()
     for l in lines:
         if l['event'] != "eth.vm.op.vm":
             continue
-        if l['pc'] == 0:
-            bnum += 1
         # remove py slogging specifics:
         l.pop('event')
         l.pop('level')
-        blocks[bnum].append(l)
-    return blocks
+        txs.append(l)
+    return read_transactions(txs)
+
+
+def read_transactions(traces):
+    txs = defaultdict(list)
+    tx_num = 0
+    for step in traces:
+        if step['pc'] == 0:
+            tx_num += 1
+        txs[tx_num].append(step)
+    return txs
+
 
 if __name__ == '__main__':
     pyblocks = read_pythons()
-    blocknum = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+    txnum = int(sys.argv[1]) if len(sys.argv) > 1 else 1
 
-    print json.dumps(pyblocks[blocknum], indent=2)
+    print json.dumps(pyblocks[txnum], indent=2)
