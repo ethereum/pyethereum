6a148022c2a2a0dd4540cf5877b345fa793bafc6
Brian S. Corbin <brian.corbin@pokitdok.com>
2016-05-05 22:29:36 -0400

    Return the result of add_transaction when it's called again due to a new head_candidate

--- ethereum/chain.py
+++ ethereum/chain.py
@@ -345,7 +345,7 @@ class Chain(object):
         log.debug('add tx', num_txs=self.num_transactions(), tx=transaction, on=head_candidate)
         if self.head_candidate.includes_transaction(transaction.hash):
             log.debug('known tx')
-            return
+            return False
         old_state_root = head_candidate.state_root
         # revert finalization
         head_candidate.state_root = self.pre_finalize_state_root
@@ -363,8 +363,7 @@ class Chain(object):
         # we might have a new head_candidate (due to ctx switches in pyethapp)
         if self.head_candidate != head_candidate:
             log.debug('head_candidate changed during validation, trying again')
-            self.add_transaction(transaction)
-            return
+            return self.add_transaction(transaction)
 
         self.pre_finalize_state_root = head_candidate.state_root
         head_candidate.finalize()
