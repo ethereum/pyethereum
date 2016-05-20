8a6e56728323b010a1e864ec2de226ec706255b8
Konrad Feldmeier <konrad.feldmeier@brainbot.com>
2016-05-20 17:30:37 +0200

    Bump version: 1.3.1 → 1.3.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = [tests_require_replacements.get(r, r) for r in tests_require]
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '1.3.1'
+version = '1.3.2'
 
 setup(
     name="ethereum",
