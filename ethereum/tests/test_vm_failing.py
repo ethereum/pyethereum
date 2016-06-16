6c6be1107c2b604419b3ae03dcb17abd489ebe12
Piper Merriam <pipermerriam@gmail.com>
2016-06-16 10:54:08 -0600

    fix non-deterministic test

--- ethereum/tests/test_vm_failing.py
+++ ethereum/tests/test_vm_failing.py
@@ -50,10 +50,17 @@ for func_name, filename, testname, testdata in collected:
 def test_testutils_check_vm_test():
     func_name, filename, testname, testdata = collected[1]
     testutils.check_vm_test(testutils.fixture_to_bytes(testdata))
-    # manipulate post data
-    storage = tuple(testdata['post'].values())[0]['storage']
-    assert storage['0x23'] == '0x01'
-    storage['0x23'] = '0x02'
+    for address_data in testdata['post'].values():
+        storage = address_data['storage']
+        if "0x23" not in storage:
+            continue
+        assert storage['0x23'] == '0x01'
+        # manipulate post data
+        storage['0x23'] = '0x02'
+        break
+    else:
+        raise AssertionError("Did not find `0x23` in storage values")
+
     failed_as_expected = False
     try:
         testutils.check_vm_test(testutils.fixture_to_bytes(testdata))
