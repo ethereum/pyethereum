8e42cbd22a32b568f62b0179e893e12d1d785c16
vub <Vitalik Buterin>
2016-07-02 08:05:04 -0400

    Added new chain class, and incorporated it into frontier test

--- ethereum/parse_genesis_declaration.py
+++ ethereum/parse_genesis_declaration.py
@@ -2,6 +2,7 @@ from ethereum.state import State, STATE_DEFAULTS
 from ethereum.block import Block, BlockHeader, FakeHeader
 from ethereum.utils import decode_hex, big_endian_to_int, encode_hex
 from ethereum.state_transition import apply_block, initialize
+import rlp
 
 def parse_as_bin(s):
     return decode_hex(s[2:] if s[:2] == '0x' else s)
@@ -118,4 +119,58 @@ def state_from_genesis_declaration(genesis_data, env):
     state.commit()
     blk.header.state_root = state.trie.root_hash
     state.prev_headers=[h]
+    return state
+
+
+def mk_basic_state(alloc, header, env):
+    state = State(env=env)
+    state["prev_headers"] = [FakeHeader(hash=parse_as_bin(header['hash']),
+                                        number=parse_as_int(header['number']),
+                                        timestamp=parse_as_int(header['timestamp']),
+                                        difficulty=parse_as_int(header['difficulty']),
+                                        gas_limit=parse_as_int(header['gas_limit']))]
+    
+    for addr, data in genesis_data["alloc"].items():
+        if len(addr) == 40:
+            addr = decode_hex(addr)
+        assert len(addr) == 20
+        if 'wei' in data:
+            state.set_balance(addr, parse_as_int(data['wei']))
+        if 'balance' in data:
+            state.set_balance(addr, parse_as_int(data['balance']))
+        if 'code' in data:
+            state.set_code(addr, parse_as_bin(data['code']))
+        if 'nonce' in data:
+            state.set_nonce(addr, parse_as_int(data['nonce']))
+        if 'storage' in data:
+            for k, v in data['storage'].items():
+                state.set_storage_data(addr, parse_as_bin(k), parse_as_bin(v))
+
+    state.block_number = header["number"]
+    state.gas_limit = header["gas_limit"]
+    state.timestamp = header["timestamp"]
+    state.block_difficulty = header["difficulty"]
+    state.commit()
+    return state
+
+
+def mk_poststate_of_block(block, env):
+    state = State(env=env)
+    state.trie.root_hash = block.header.state_root
+    initialize(state, block)
+    state.gas_used = block.header.gas_used
+    state.txindex = len(block.transactions)
+    state.recent_uncles = {}
+    state.prev_headers = []
+    b = block
+    for i in range(257):
+        state.prev_headers.append(b.header)
+        if i < 6:
+            state.recent_uncles[state.block_number - i] = []
+            for u in b.uncles:
+                state.recent_uncles[state.block_number - i].append(u.hash)
+        try:
+            b = rlp.decode(state.db.get(b.header.prevhash), Block)
+        except:
+            break
     return state
--- ethereum/tests/test_frontier.py
+++ ethereum/tests/test_frontier.py
@@ -2,6 +2,7 @@ from ethereum import parse_genesis_declaration, db
 from ethereum.block import Block
 from ethereum.config import Env
 from ethereum.state_transition import apply_block
+from ethereum import chain
 import rlp
 import json
 import os
@@ -12,33 +13,35 @@ import sys
 # configure_logging(config_string=config_string)
 
 if 'saved_state.json' in os.listdir(os.getcwd()):
-    s = parse_genesis_declaration.state_from_snapshot(json.load(open('saved_state.json')), Env())
+    c = chain.Chain(json.load(open('saved_state.json')), Env())
     print 'state generated from saved state'
 elif 'genesis_frontier.json' not in os.listdir(os.getcwd()):
     print 'Please download genesis_frontier.json from http://vitalik.ca/files/genesis_frontier.json'
     sys.exit()
 else:
-    s = parse_genesis_declaration.state_from_genesis_declaration(json.load(open('genesis_frontier.json')), Env())
-    assert s.trie.root_hash.encode('hex') == 'd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544'
-    assert s.prev_headers[0].hash.encode('hex') == 'd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
+    c = chain.Chain(json.load(open('genesis_frontier.json')), Env())
+    assert c.state.trie.root_hash.encode('hex') == 'd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544'
+    assert c.state.prev_headers[0].hash.encode('hex') == 'd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
     print 'state generated from genesis'
 if '200kblocks.rlp' not in os.listdir(os.getcwd()):
     print 'Please download 200kblocks.rlp from http://vitalik.ca/files/200kblocks.rlp and put it in this directory to continue the test'
     sys.exit()
-block_rlps = open('200kblocks.rlp').readlines()[s.block_number + 1:]
+block_rlps = open('200kblocks.rlp').readlines()[c.state.block_number + 1:]
 for block in block_rlps:
     # print 'prevh:', s.prev_headers
     block = rlp.decode(block.strip().decode('hex'), Block)
-    apply_block(s, block)
+    assert c.add_block(block)
     if block.header.number % 1000 == 0:
-        snapshot = parse_genesis_declaration.to_snapshot(s)
-        s = parse_genesis_declaration.state_from_snapshot(snapshot, s.env)
-        snapshot2 = parse_genesis_declaration.to_snapshot(s)
+        snapshot = parse_genesis_declaration.to_snapshot(c.state)
+        if block.header.number % 2000 == 0:
+            c = chain.Chain(env=c.env)
+        else:
+            c = chain.Chain(snapshot, Env())
+        snapshot2 = parse_genesis_declaration.to_snapshot(c.state)
         if snapshot != snapshot2:
             open('/tmp/1', 'w').write(json.dumps(snapshot))
             open('/tmp/2', 'w').write(json.dumps(snapshot2))
             raise Exception("snapshot difference")
         open('saved_state.json', 'w').write(json.dumps(snapshot, indent=4))
-    print 'Applied block %d, %d transactions %d gas %d limit' % (block.header.number, len(block.transactions), block.header.gas_used, block.header.gas_limit)
 
 print 'Test successful'
