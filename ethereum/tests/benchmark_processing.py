c5a27eb9c5898060ba6a7d272f4c3a359c551938
Jan Xie <jan.h.xie@gmail.com>
2016-07-17 16:03:20 +0800

    5000 blocks limit and benchmark results

--- ethereum/tests/benchmark_processing.py
+++ ethereum/tests/benchmark_processing.py
@@ -25,9 +25,6 @@ else:
     assert c.state.trie.root_hash.encode('hex') == 'd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544'
     assert c.state.prev_headers[0].hash.encode('hex') == 'd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
     print 'state generated from genesis'
-if '200kblocks.rlp' not in os.listdir(os.getcwd()):
-    print 'Please download 200kblocks.rlp from http://vitalik.ca/files/200kblocks.rlp and put it in this directory to continue the test'
-    sys.exit()
 
 batch_size = 1024 * 10240 # approximately 10000 blocks
 f = open('1700kblocks.rlp')
@@ -58,6 +55,8 @@ if not validate_receipt_root:
         return True
     state_transition.validate_receipt_root = validate_receipt_root
 
+limit = int(os.environ.get('LIMIT', '5000'))
+
 processed_blocks = 0
 processed_txs = 0
 processed_gas = 0
@@ -77,5 +76,23 @@ while len(block_rlps) > 0:
             (elapsed,
              processed_blocks, processed_txs, processed_gas,
              processed_blocks/elapsed, processed_txs/elapsed, processed_gas/elapsed)
+
+        if processed_blocks == limit:
+            print "benchmark finished."
+            sys.exit()
+
     block_rlps = f.readlines(batch_size)
 
+##
+# Result (3 rounds, start from Block#1000k)
+#
+# default:
+#   318.812145948 >>> processed 5000 blocks, 17231 txs, 528752649 gas, avg. 15.68bps, 54.05tps 1658508.48gps
+#   317.192508936 >>> processed 5000 blocks, 17231 txs, 528752649 gas, avg. 15.76bps, 54.32tps 1666977.10gps
+#   322.749845982 >>> processed 5000 blocks, 17231 txs, 528752649 gas, avg. 15.49bps, 53.39tps 1638273.90gps
+#
+# INCLUDE_DB_COMMIT=1 VALIDATE_RECEIPT_ROOT=1
+#   357.856366873 >>> processed 5000 blocks, 17231 txs, 528752649 gas, avg. 13.97bps, 48.15tps 1477555.52gps
+#   329.664104939 >>> processed 5000 blocks, 17231 txs, 528752649 gas, avg. 15.17bps, 52.27tps 1603913.32gps
+#   368.661725998 >>> processed 5000 blocks, 17231 txs, 528752649 gas, avg. 13.56bps, 46.74tps 1434248.83gps
+#
