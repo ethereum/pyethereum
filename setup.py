942d009bd4231cd463157818f19d3a921a58b30e
Konrad Feldmeier <konrad@brainbot.com>
2016-10-13 15:43:10 +0200

    Bump version: 1.5.3 → 1.5.4
    Changelog:
    - add option to set `SOLC_BINARY` from environment #407
    - allow extra args for `solc_wrapper` #410

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [tests_require_replacements.get(r, r) for r in tests_require]
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '1.5.3'
+version = '1.5.4'
 
 setup(
     name="ethereum",
