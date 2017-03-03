2ac2a1d9ced653f0bb07205ddc55ff781054916c
Konrad Feldmeier <konrad@brainbot.com>
2017-03-03 08:39:17 +0100

    Fix fat finger errors and echo solc version

--- .travis/install_solc.sh
+++ .travis/install_solc.sh
@@ -7,7 +7,7 @@ set -e
 set -u
 
 mkdir -p $HOME/solc-versions/solc-$SOLC_VERSION
-cd solc-versions/solc-$SOLC_VERSION
+cd $HOME/solc-versions/solc-$SOLC_VERSION
 
 if [ ! -f solc ]
 then
@@ -15,8 +15,8 @@ then
     ./solidity/scripts/install_deps.sh
     wget https://github.com/ethereum/solidity/releases/download/v$SOLC_VERSION/solidity-ubuntu-trusty.zip
     unzip solidity-ubuntu-trusty.zip
-    echo "Solidity installed at $TRAVIS_BUILD_DIR/solc-versions/solc-$SOLC_VERSION/solc"
-    tree $TRAVIS_BUILD_DIR/solc-versions/solc-$SOLC_VERSION
+    echo "Solidity installed at $HOME/solc-versions/solc-$SOLC_VERSION/solc"
+    tree $HOME/solc-versions/solc-$SOLC_VERSION
 fi
 
 if [ -f $HOME/.bin/solc ]
@@ -24,4 +24,10 @@ then
     rm $HOME/.bin/solc
 fi
 
-ln -s $HOME/solc-versions/solc-$SOLC_VERSION $HOME/.bin/solc
+ln -s $HOME/solc-versions/solc-$SOLC_VERSION/solc $HOME/.bin/solc
+# Check path is correctly set up and echo version
+cd
+echo $PATH
+ls -al $HOME/.bin
+ls -al $(readlink -f $HOME/.bin/solc)
+solc --version
