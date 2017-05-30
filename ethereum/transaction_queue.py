721b9f6ef7d1b7f28ce34ba4a7defea0feeacaf5
Guilherme Salgado <gsalgado@gmail.com>
2017-05-30 11:39:55 +0100

    Fix TransactionQueue.diff()

--- ethereum/transaction_queue.py
+++ ethereum/transaction_queue.py
@@ -40,17 +40,19 @@ class TransactionQueue():
 
     def diff(self, txs):
         remove_hashes = [tx.hash for tx in txs]
-        keep = [tx for tx in self.txs if tx.hash not in remove_hashes]
+        keep = [(prio, tx) for (prio, tx) in self.txs if tx.hash not in remove_hashes]
         q = TransactionQueue()
-        for tx in keep:
-            q.add_transaction(tx)
+        q.txs = keep
         return q
 
-def test():
+
+def make_test_tx(s=100000, g=50, data=''):
     from ethereum.transactions import Transaction
-    def tx(s, g):
-        return Transaction(nonce=0, startgas=s, gasprice=g,
-                           value=0, data='', to='\x35' * 20)
+    return Transaction(nonce=0, startgas=s, gasprice=g,
+                       value=0, data=data, to='\x35' * 20)
+
+
+def test():
     q = TransactionQueue()
     # (startgas, gasprice) pairs
     params = [(100000, 81), (50000, 74), (40000, 65),
@@ -66,7 +68,7 @@ def test():
                   (999999, 50000, 74)]
     # Add transactions to queue
     for param in params:
-        q.add_transaction(tx(param[0], param[1]))
+        q.add_transaction(make_test_tx(s=param[0], g=param[1]))
     # Attempt pops from queue
     for (maxgas, expected_s, expected_g) in operations:
         tx = q.pop_transaction(max_gas=maxgas)
@@ -75,3 +77,23 @@ def test():
         else:
             assert expected_s is expected_g is None
     print('Test successful')
+
+
+def test_diff():
+    tx1 = make_test_tx(data='foo')
+    tx2 = make_test_tx(data='bar')
+    tx3 = make_test_tx(data='baz')
+    tx4 = make_test_tx(data='foobar')
+    q1 = TransactionQueue()
+    for tx in [tx1, tx2, tx3, tx4]:
+        q1.add_transaction(tx)
+    q2 = q1.diff([tx2])
+    assert len(q2) == 3
+    assert tx1 in [tx for (_, tx) in q2.txs]
+    assert tx3 in [tx for (_, tx) in q2.txs]
+    assert tx4 in [tx for (_, tx) in q2.txs]
+
+    q3 = q2.diff([tx4])
+    assert len(q3) == 2
+    assert tx1 in [tx for (_, tx) in q3.txs]
+    assert tx3 in [tx for (_, tx) in q3.txs]
