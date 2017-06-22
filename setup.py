267a534ac65d4d0b928942ce0c1cc4b45cd2badf
Vitalik Buterin <v@buterin.com>
2017-06-22 02:26:13 -0400

    Bump version: 1.6.1 → 2.0.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [tests_require_replacements.get(r, r) for r in tests_require]
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '1.6.1'
+version = '2.0.0'
 
 setup(
     name="ethereum",
