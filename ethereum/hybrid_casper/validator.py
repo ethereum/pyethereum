d555cdea8d91415589cbb9f81796cb590184738e
Karl Floersch <karl@karlfloersch.com>
2017-07-04 18:06:21 -0600

    Finish basic validator

--- ethereum/hybrid_casper/validator.py
+++ ethereum/hybrid_casper/validator.py
@@ -7,7 +7,9 @@ from ethereum import block, transactions
 from ethereum.transaction_queue import TransactionQueue
 from ethereum.messages import apply_transaction
 from ethereum import abi, utils
-# from ethereum.slogging import get_logger, configure_logging
+from ethereum.slogging import get_logger
+
+log = get_logger('eth.validator')
 # config_string = ':info,eth.vm.log:trace,eth.vm.op:trace,eth.vm.stack:trace,eth.vm.exit:trace,eth.pb.msg:trace,eth.pb.tx:debug'
 # configure_logging(config_string=config_string)
 
@@ -37,7 +39,6 @@ class Validator(object):
         self.prepares = dict()
         self.prev_commit_epoch = 0
         self.epoch_length = self.chain.env.config['EPOCH_LENGTH']
-        # self.commits = dict()
         # When the transaction_queue is modified, we must set
         # self._head_candidate_needs_updating to True in order to force the
         # head candidate to be updated.
@@ -77,6 +78,7 @@ class Validator(object):
 
     def get_validator_index(self, state):
         t = tester.State(state.ephemeral_clone())
+        t.state.gas_limit = 9999999999
         casper = tester.ABIContract(t, casper_utils.casper_abi, self.chain.casper_address)
         try:
             return casper.get_validator_indexes(self.valcode_addr)
@@ -93,7 +95,6 @@ class Validator(object):
     def accept_block(self, block):
         self.chain.process_parent_queue()
         if not self.chain.add_block(block):
-            print('Block failed to add:', block.hash)
             return
         # Create a poststate based on the blockhash we recieved
         post_state = self.chain.mk_poststate_of_blockhash(block.hash)
@@ -106,22 +107,27 @@ class Validator(object):
         if commit_msg:
             commit_tx = self.mk_commit_tx(commit_msg)
             self.broadcast_transaction(commit_tx)
-        print('Block added.', block.hash, 'Head:', self.chain.head_hash)
+        if self.valcode_addr:
+            code = self.chain.state.get_code(self.valcode_addr)
+            log.info('Validator: {} - Code: {}'.format(utils.encode_hex(self.coinbase), code))
 
     def accept_transaction(self, tx):
         if self.mining:
+            log.info('Recieved tx: {}'.format(tx))
             self.transaction_queue.add_transaction(tx)
             self.mine_and_broadcast_blocks(1)
 
     def broadcast_transaction(self, tx):
+        log.info('Broadcasting transaction {} from validator {}'.format(str(tx), utils.encode_hex(self.valcode_addr)))
         self.network.broadcast(tx)
 
     def broadcast_newblock(self, block):
-        print('Block hash:', block.hash)
-        print('Block txs:', block.transactions)
+        log.info('Broadcasting block with hash: %s and txs: %s' % (utils.encode_hex(block.hash), str(block.transactions)))
         self.network.broadcast(block)
 
     def get_prepare_message(self, state):
+        # TODO: Only prepare on head fork
+        state.gas_limit = 9999999999999
         epoch = state.block_number // self.epoch_length
         # Don't prepare if we have already or if we don't have EPOCH_LENGTH/3 or more blocks in this epoch
         if epoch in self.prepares or state.block_number % self.epoch_length < self.epoch_length // 3:
@@ -143,11 +149,16 @@ class Validator(object):
         try:  # Attempt to submit the prepare, to make sure that it doesn't doesn't violate DBL_PREPARE & it is justified
             casper.prepare(prepare_msg)
         except tester.TransactionFailed:
-            print('Prepare failed')
+            log.info('Prepare failed! Validator {} - hash justified {} - validator start {} - valcode addr {}'
+                     .format(self.get_validator_index(state),
+                             casper.get_consensus_messages__ancestry_hash_justified(epoch, ancestry_hash),
+                             casper.get_validators__dynasty_start(self.get_validator_index(state)),
+                             utils.encode_hex(self.valcode_addr)))
             return False
         # Save the prepare message we generated
         self.prepares[epoch] = prepare_msg
-        print('Submitted Prepare:', self.get_validator_index(state), epoch, self.epoch_blockhash(state, epoch), self.prev_commit_epoch)
+        log.info('Prepare submitted: validator %d - epoch %d - prev_commit_epoch %d - hash %s' %
+                 (self.get_validator_index(state), epoch, self.prev_commit_epoch, utils.encode_hex(self.epoch_blockhash(state, epoch))))
         return prepare_msg
 
     def get_commit_message(self, state):
@@ -155,6 +166,7 @@ class Validator(object):
         # Don't commit if we have already or if we don't have EPOCH_LENGTH/2 or more blocks in this epoch
         if state.block_number % self.epoch_length < self.epoch_length // 2:
             return False
+        state.gas_limit = 9999999999999
         # Create a Casper contract which we can use to get related values
         casper = tester.ABIContract(tester.State(state), casper_utils.casper_abi, self.chain.casper_address)
         # Make the commit message
@@ -163,20 +175,21 @@ class Validator(object):
         try:  # Attempt to submit the commit, to make sure that it doesn't doesn't violate DBL_PREPARE & it is justified
             casper.commit(commit_msg)
         except tester.TransactionFailed:
-            print('Commit failed')
+            log.info('Commit failed! Validator {} - blockhash {} - valcode addr {}'
+                     .format(self.get_validator_index(state),
+                             self.epoch_blockhash(state, epoch),
+                             utils.encode_hex(self.valcode_addr)))
             return False
         # Save the commit as now our last commit epoch
+        log.info('Commit submitted: validator %d - epoch %d - prev_commit_epoch %d - hash %s' %
+                 (self.get_validator_index(state), epoch, self.prev_commit_epoch, utils.encode_hex(self.epoch_blockhash(state, epoch))))
         self.prev_commit_epoch = epoch
-        print('Submitted Commit:', self.get_validator_index(state), epoch, self.epoch_blockhash(state, epoch), self.prev_commit_epoch)
         return commit_msg
 
     def mine_and_broadcast_blocks(self, number_of_blocks=1):
         for i in range(number_of_blocks):
             self._head_candidate_needs_updating = True
             block = Miner(self.head_candidate).mine(rounds=100, start_nonce=0)
-            print('Did we add block? ', self.chain.add_block(block))
-            print('mining a new block with hash:', utils.encode_hex(block.hash), '\nand prevhash:', utils.encode_hex(block.header.prevhash))
-            print('mining a new block my head is:', utils.encode_hex(self.chain.head_hash))
             self.transaction_queue = self.transaction_queue.diff(block.transactions)
             self.broadcast_newblock(block)
 
@@ -184,24 +197,27 @@ class Validator(object):
         if not self.valcode_tx or not self.deposit_tx:
             # Generate transactions
             valcode_tx = self.mk_validation_code_tx()
-            valcode_addr = utils.mk_contract_address(self.coinbase, self.nonce)
+            valcode_addr = utils.mk_contract_address(self.coinbase, self.nonce-1)
             deposit_tx = self.mk_deposit_tx(3 * 10**18, valcode_addr)
             # Verify the transactions pass
             temp_state = self.chain.state.ephemeral_clone()
             valcode_success, o1 = apply_transaction(temp_state, valcode_tx)
             deposit_success, o2 = apply_transaction(temp_state, deposit_tx)
-            print(valcode_success)
-            print(deposit_success)
             if not (valcode_success and deposit_success):
                 self.nonce = self.chain.state.get_nonce(self.coinbase)
-                print(o1)
-                print(o2)
                 raise Exception('Valcode tx or deposit tx failed')
             self.valcode_tx = valcode_tx
+            log.info('Valcode Tx generated: {}'.format(str(valcode_tx)))
             self.valcode_addr = valcode_addr
             self.deposit_tx = deposit_tx
+            log.info('Deposit Tx generated: {}'.format(str(deposit_tx)))
         self.broadcast_transaction(self.valcode_tx)
         self.broadcast_transaction(self.deposit_tx)
+
+    def mk_transaction(self, to=b'\x00' * 20, value=0, data=b'', gasprice=tester.GASPRICE, startgas=tester.STARTGAS):
+        tx = transactions.Transaction(self.nonce, gasprice, startgas, to, value, data).sign(self.key)
+        self.nonce += 1
+        return tx
 
     def mk_validation_code_tx(self):
         valcode_tx = self.mk_transaction('', 0, casper_utils.mk_validation_code(self.coinbase))
@@ -224,8 +240,3 @@ class Validator(object):
         commit_func = casper_ct.encode('commit', [commit_msg])
         commit_tx = self.mk_transaction(self.chain.casper_address, 0, commit_func)
         return commit_tx
-
-    def mk_transaction(self, to=b'\x00' * 20, value=0, data=b'', gasprice=tester.GASPRICE, startgas=tester.STARTGAS):
-        tx = transactions.Transaction(self.nonce, gasprice, startgas, to, value, data).sign(self.key)
-        self.nonce += 1
-        return tx
