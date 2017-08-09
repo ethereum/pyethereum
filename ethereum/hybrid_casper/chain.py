a748c6fae323ed689fd9a28c8701784b09e4f4df
Karl Floersch <karl@karlfloersch.com>
2017-08-09 00:06:49 -0400

    Reorganize functions

--- ethereum/hybrid_casper/chain.py
+++ ethereum/hybrid_casper/chain.py
@@ -72,7 +72,7 @@ class Chain(object):
         self.head_hash = self.state.prev_headers[0].hash
         self.checkpoint_head_hash = b'\x00' * 32
         self.commit_logs = []
-        self.casper_address = self.env.config['CASPER_ADDRESS']
+        self.casper_address = self.config['CASPER_ADDRESS']
         self.db.put('GENESIS_NUMBER', str(self.state.block_number))
         assert self.state.block_number == self.state.prev_headers[0].number
         if reset_genesis:
@@ -99,8 +99,7 @@ class Chain(object):
             log.error(e)
             return None
 
-    # Casper fork choice outline:
-    # TODO
+    # ~~~~~~~~~~~~~~~~~~~~ CASPER ~~~~~~~~~~~~~~~~~~~~ #
 
     def casper_log_handler(self, contract_log, fork_state, blockhash):
         # We only want logs from the Casper contract
@@ -134,36 +133,6 @@ class Chain(object):
             self.db.put(b'cp_deposits:' + checkpoint_hash, deposits)
             # Update the checkpoint_head_hash if needed
             self.maybe_update_checkpoint_head_hash(commit['hash'])
-
-    def get_decoded_commit(self, commit_rlp):
-        commit_array = rlp.decode(commit_rlp)
-        commit = dict()
-        for i, field in enumerate(['validator_index', 'epoch', 'hash', 'prev_commit_epoch', 'sig']):
-            commit[field] = commit_array[i]
-        commit['raw_rlp'] = commit_rlp
-        return commit
-
-    def get_prev_checkpoint_block(self, block):
-        epoch_length = self.env.config['EPOCH_LENGTH']
-        checkpoint_distance = (block.header.number) % epoch_length
-        if checkpoint_distance == 0:
-            checkpoint_distance = epoch_length
-        for i in range(checkpoint_distance):
-            if block.header.prevhash == b'\x00' * 32:
-                return block
-            block = self.get_block(block.header.prevhash)
-        return block
-
-    def is_parent_checkpoint(self, parent, child):
-        parent_block = self.get_block(parent)
-        child_block = self.get_block(child)
-        while parent_block.header.number < child_block.header.number:
-            log.info('child block num: {}'.format(child_block.header.number))
-            child_block = self.get_prev_checkpoint_block(child_block)
-        if parent_block == child_block:
-            return True
-        else:
-            return False
 
     def maybe_update_checkpoint_head_hash(self, fork_hash):
         # If our checkpoint head is the initial head hash value, immediately use the fork hash
@@ -208,141 +177,7 @@ class Chain(object):
         # Otherwise, return False (continue to use the head)
         return False
 
-    def get_checkpoint_score(self, checkpoint_hash):
-        try:
-            total_deposits = self.db.get(b'cp_total_deposits:' + checkpoint_hash)
-        except KeyError:
-            return 0
-        prev_dyn_total_deposits, curr_dyn_total_deposits = utils.big_endian_to_int(total_deposits[:32]), utils.big_endian_to_int(total_deposits[32:])
-        deposits = self.db.get(b'cp_deposits:' + checkpoint_hash)
-        prev_dyn_deposits = 0
-        curr_dyn_deposits = 0
-        for key, d in deposits.items():
-            prev_dyn_deposits += utils.big_endian_to_int(d[:32])
-            curr_dyn_deposits += utils.big_endian_to_int(d[32:])
-        prev_dyn_score = prev_dyn_deposits / prev_dyn_total_deposits if prev_dyn_total_deposits > 0 else 1
-        curr_dyn_score = curr_dyn_deposits / curr_dyn_total_deposits if curr_dyn_total_deposits > 0 else 1
-        return min(prev_dyn_score, curr_dyn_score)
-
-    def mk_poststate_of_blockhash(self, blockhash, convert=False):
-        if blockhash not in self.db:
-            raise Exception("Block hash %s not found" % encode_hex(blockhash))
-
-        block_rlp = self.db.get(blockhash)
-        if block_rlp == 'GENESIS':
-            return State.from_snapshot(json.loads(self.db.get('GENESIS_STATE')), self.env)
-        block = rlp.decode(block_rlp, Block)
-
-        state = State(env=self.env)
-        state.trie.root_hash = block.header.state_root if convert else self.db.get(b'state:'+blockhash)
-        update_block_env_variables(state, block)
-        state.gas_used = block.header.gas_used
-        state.txindex = len(block.transactions)
-        state.recent_uncles = {}
-        state.prev_headers = []
-        b = block
-        header_depth = state.config['PREV_HEADER_DEPTH']
-        for i in range(header_depth + 1):
-            state.prev_headers.append(b.header)
-            if i < 6:
-                state.recent_uncles[state.block_number - i] = []
-                for u in b.uncles:
-                    state.recent_uncles[state.block_number - i].append(u.hash)
-            try:
-                b = rlp.decode(state.db.get(b.header.prevhash), Block)
-            except:
-                break
-        if i < header_depth:
-            if state.db.get(b.header.prevhash) == 'GENESIS':
-                jsondata = json.loads(state.db.get('GENESIS_STATE'))
-                for h in jsondata["prev_headers"][:header_depth - i]:
-                    state.prev_headers.append(dict_to_prev_header(h))
-                for blknum, uncles in jsondata["recent_uncles"].items():
-                    if int(blknum) >= state.block_number - int(state.config['MAX_UNCLE_DEPTH']):
-                        state.recent_uncles[blknum] = [parse_as_bin(u) for u in uncles]
-            else:
-                raise Exception("Dangling prevhash")
-        assert len(state.journal) == 0, state.journal
-        return state
-
-    def get_parent(self, block):
-        if block.header.number == int(self.db.get('GENESIS_NUMBER')):
-            return None
-        return self.get_block(block.header.prevhash)
-
-    def get_block(self, blockhash):
-        try:
-            block_rlp = self.db.get(blockhash)
-            if block_rlp == 'GENESIS':
-                if not hasattr(self, 'genesis'):
-                    self.genesis = rlp.decode(self.db.get('GENESIS_RLP'), sedes=Block)
-                return self.genesis
-            else:
-                return rlp.decode(block_rlp, Block)
-        except Exception as e:
-            log.debug("Failed to get block", hash=blockhash, error=e)
-            return None
-
-    # Add a record allowing you to later look up the provided block's
-    # parent hash and see that it is one of its children
-    def add_child(self, child):
-        try:
-            existing = self.db.get(b'child:' + child.header.prevhash)
-        except:
-            existing = b''
-        existing_hashes = []
-        for i in range(0, len(existing), 32):
-            existing_hashes.append(existing[i: i+32])
-        if child.header.hash not in existing_hashes:
-            self.db.put(b'child:' + child.header.prevhash, existing + child.header.hash)
-
-    def get_blockhash_by_number(self, number):
-        try:
-            return self.db.get('block:' + str(number))
-        except:
-            return None
-
-    def get_block_by_number(self, number):
-        return self.get_block(self.get_blockhash_by_number(number))
-
-    # Get the hashes of all known children of a given block
-    def get_child_hashes(self, blockhash):
-        o = []
-        try:
-            data = self.db.get(b'child:' + blockhash)
-            for i in range(0, len(data), 32):
-                o.append(data[i:i + 32])
-            return o
-        except:
-            return []
-
-    def get_children(self, block):
-        if isinstance(block, Block):
-            block = block.header.hash
-        if isinstance(block, BlockHeader):
-            block = block.hash
-        return [self.get_block(h) for h in self.get_child_hashes(block)]
-
-    # Get the score (AKA total difficulty in PoW) of a given block
-    def get_score(self, block):
-        if not block:
-            return 0
-        key = b'score:' + block.header.hash
-
-        fills = []
-        while key not in self.db:
-            fills.insert(0, (block.header.hash, block.difficulty))
-            key = b'score:' + block.header.prevhash
-            block = self.get_parent(block)
-            if block is None:
-                return 0
-        score = int(self.db.get(key))
-        for h, d in fills:
-            key = b'score:' + h
-            score = score + d + random.randrange(d // 10**6 + 1)
-            self.db.put(key, str(score))
-
-        return score
+    # ~~~~~~~~~~~~~~~~~~~~ ADD BLOCK ~~~~~~~~~~~~~~~~~~~~ #
 
     # This function should be called periodically so as to
     # process blocks that were received but laid aside because
@@ -486,18 +321,175 @@ class Chain(object):
         self.head_hash = block.header.hash
         self.state = self.mk_poststate_of_blockhash(block.hash)
 
-    def __contains__(self, blk):
-        if isinstance(blk, (str, bytes)):
-            try:
-                blk = rlp.decode(self.db.get(blk), Block)
-            except:
-                return False
-        try:
-            o = self.get_block(self.get_blockhash_by_number(blk.number)).hash
-            assert o == blk.hash
+    # ~~~~~~~~~~~~~~~~~~~~ CASPER UTILS ~~~~~~~~~~~~~~~~~~~~ #
+
+    def get_decoded_commit(self, commit_rlp):
+        commit_array = rlp.decode(commit_rlp)
+        commit = dict()
+        for i, field in enumerate(['validator_index', 'epoch', 'hash', 'prev_commit_epoch', 'sig']):
+            commit[field] = commit_array[i]
+        commit['raw_rlp'] = commit_rlp
+        return commit
+
+    def get_prev_checkpoint_block(self, block):
+        epoch_length = self.env.config['EPOCH_LENGTH']
+        checkpoint_distance = (block.header.number) % epoch_length
+        if checkpoint_distance == 0:
+            checkpoint_distance = epoch_length
+        for i in range(checkpoint_distance):
+            if block.header.prevhash == b'\x00' * 32:
+                return block
+            block = self.get_block(block.header.prevhash)
+        return block
+
+    def is_parent_checkpoint(self, parent, child):
+        parent_block = self.get_block(parent)
+        child_block = self.get_block(child)
+        while parent_block.header.number < child_block.header.number:
+            log.info('child block num: {}'.format(child_block.header.number))
+            child_block = self.get_prev_checkpoint_block(child_block)
+        if parent_block == child_block:
             return True
-        except:
+        else:
             return False
+
+    def get_checkpoint_score(self, checkpoint_hash):
+        try:
+            total_deposits = self.db.get(b'cp_total_deposits:' + checkpoint_hash)
+        except KeyError:
+            return 0
+        prev_dyn_total_deposits, curr_dyn_total_deposits = utils.big_endian_to_int(total_deposits[:32]), utils.big_endian_to_int(total_deposits[32:])
+        deposits = self.db.get(b'cp_deposits:' + checkpoint_hash)
+        prev_dyn_deposits = 0
+        curr_dyn_deposits = 0
+        for key, d in deposits.items():
+            prev_dyn_deposits += utils.big_endian_to_int(d[:32])
+            curr_dyn_deposits += utils.big_endian_to_int(d[32:])
+        prev_dyn_score = prev_dyn_deposits / prev_dyn_total_deposits if prev_dyn_total_deposits > 0 else 1
+        curr_dyn_score = curr_dyn_deposits / curr_dyn_total_deposits if curr_dyn_total_deposits > 0 else 1
+        return min(prev_dyn_score, curr_dyn_score)
+
+    # ~~~~~~~~~~~~~~~~~~~~ BLOCK UTILS ~~~~~~~~~~~~~~~~~~~~ #
+
+    def mk_poststate_of_blockhash(self, blockhash, convert=False):
+        if blockhash not in self.db:
+            raise Exception("Block hash %s not found" % encode_hex(blockhash))
+
+        block_rlp = self.db.get(blockhash)
+        if block_rlp == 'GENESIS':
+            return State.from_snapshot(json.loads(self.db.get('GENESIS_STATE')), self.env)
+        block = rlp.decode(block_rlp, Block)
+
+        state = State(env=self.env)
+        state.trie.root_hash = block.header.state_root if convert else self.db.get(b'state:'+blockhash)
+        update_block_env_variables(state, block)
+        state.gas_used = block.header.gas_used
+        state.txindex = len(block.transactions)
+        state.recent_uncles = {}
+        state.prev_headers = []
+        b = block
+        header_depth = state.config['PREV_HEADER_DEPTH']
+        for i in range(header_depth + 1):
+            state.prev_headers.append(b.header)
+            if i < 6:
+                state.recent_uncles[state.block_number - i] = []
+                for u in b.uncles:
+                    state.recent_uncles[state.block_number - i].append(u.hash)
+            try:
+                b = rlp.decode(state.db.get(b.header.prevhash), Block)
+            except:
+                break
+        if i < header_depth:
+            if state.db.get(b.header.prevhash) == 'GENESIS':
+                jsondata = json.loads(state.db.get('GENESIS_STATE'))
+                for h in jsondata["prev_headers"][:header_depth - i]:
+                    state.prev_headers.append(dict_to_prev_header(h))
+                for blknum, uncles in jsondata["recent_uncles"].items():
+                    if int(blknum) >= state.block_number - int(state.config['MAX_UNCLE_DEPTH']):
+                        state.recent_uncles[blknum] = [parse_as_bin(u) for u in uncles]
+            else:
+                raise Exception("Dangling prevhash")
+        assert len(state.journal) == 0, state.journal
+        return state
+
+    def get_parent(self, block):
+        if block.header.number == int(self.db.get('GENESIS_NUMBER')):
+            return None
+        return self.get_block(block.header.prevhash)
+
+    def get_block(self, blockhash):
+        try:
+            block_rlp = self.db.get(blockhash)
+            if block_rlp == 'GENESIS':
+                if not hasattr(self, 'genesis'):
+                    self.genesis = rlp.decode(self.db.get('GENESIS_RLP'), sedes=Block)
+                return self.genesis
+            else:
+                return rlp.decode(block_rlp, Block)
+        except Exception as e:
+            log.debug("Failed to get block", hash=blockhash, error=e)
+            return None
+
+    # Add a record allowing you to later look up the provided block's
+    # parent hash and see that it is one of its children
+    def add_child(self, child):
+        try:
+            existing = self.db.get(b'child:' + child.header.prevhash)
+        except:
+            existing = b''
+        existing_hashes = []
+        for i in range(0, len(existing), 32):
+            existing_hashes.append(existing[i: i+32])
+        if child.header.hash not in existing_hashes:
+            self.db.put(b'child:' + child.header.prevhash, existing + child.header.hash)
+
+    def get_blockhash_by_number(self, number):
+        try:
+            return self.db.get('block:' + str(number))
+        except:
+            return None
+
+    def get_block_by_number(self, number):
+        return self.get_block(self.get_blockhash_by_number(number))
+
+    # Get the hashes of all known children of a given block
+    def get_child_hashes(self, blockhash):
+        o = []
+        try:
+            data = self.db.get(b'child:' + blockhash)
+            for i in range(0, len(data), 32):
+                o.append(data[i:i + 32])
+            return o
+        except:
+            return []
+
+    def get_children(self, block):
+        if isinstance(block, Block):
+            block = block.header.hash
+        if isinstance(block, BlockHeader):
+            block = block.hash
+        return [self.get_block(h) for h in self.get_child_hashes(block)]
+
+    # Get the score (AKA total difficulty in PoW) of a given block
+    def get_score(self, block):
+        if not block:
+            return 0
+        key = b'score:' + block.header.hash
+
+        fills = []
+        while key not in self.db:
+            fills.insert(0, (block.header.hash, block.difficulty))
+            key = b'score:' + block.header.prevhash
+            block = self.get_parent(block)
+            if block is None:
+                return 0
+        score = int(self.db.get(key))
+        for h, d in fills:
+            key = b'score:' + h
+            score = score + d + random.randrange(d // 10**6 + 1)
+            self.db.put(key, str(score))
+
+        return score
 
     def has_block(self, block):
         return block in self
@@ -537,10 +529,6 @@ class Chain(object):
             output.append(b)
         return output
 
-    @property
-    def db(self):
-        return self.env.db
-
     # Get blockhashes starting from a hash and going backwards
     def get_blockhashes_from_hash(self, hash, max):
         block = self.get_block(hash)
@@ -560,6 +548,23 @@ class Chain(object):
                 break
         return hashes
 
+    def __contains__(self, blk):
+        if isinstance(blk, (str, bytes)):
+            try:
+                blk = rlp.decode(self.db.get(blk), Block)
+            except:
+                return False
+        try:
+            o = self.get_block(self.get_blockhash_by_number(blk.number)).hash
+            assert o == blk.hash
+            return True
+        except:
+            return False
+
     @property
     def config(self):
         return self.env.config
+
+    @property
+    def db(self):
+        return self.env.db
