c9cad5f29f01d06861df33f41358d9143ef11796
Karl Floersch <karl@karlfloersch.com>
2017-09-02 17:27:15 -0400

    Fix test longest known checkpoint chain

--- ethereum/tests/hybrid_casper/test_chain.py
+++ ethereum/tests/hybrid_casper/test_chain.py
@@ -168,31 +168,32 @@ def test_head_change_to_longest_known_checkpoint_chain(db):
     Test that when we change to a new checkpoint, we use the longest chain known that
     derives from that checkpoint
 
-    Chain0: 3A_5, 4A_1,                 HEAD_CHANGE
+    Chain0: 3A_5, 4A_2,                 HEAD_CHANGE
     add
     Chain1:             5B_2,           HEAD_CHANGE
     add
     Chain2:       4A_2,                 HEAD_CHANGE
     """
     keys = tester.keys[:5]
+    validator_indexes = list(range(0, 5))
     t, casper = init_multi_validator_chain_and_casper(keys)
-    epoch_1_anchash = utils.sha3(epoch_blockhash(t, 1) + epoch_blockhash(t, 0))
-    epoch_2_anchash = utils.sha3(epoch_blockhash(t, 2) + epoch_1_anchash)
     # 3A_5: Prepare and commit all
+    _e, _a, _se, _sa, _pce = get_recommended_casper_msg_contents(casper, validator_indexes)
     for i, k in enumerate(keys):
-        casper.prepare(mk_prepare(i, 3, epoch_2_anchash, 2, epoch_2_anchash, k))
+        casper.prepare(mk_prepare(i, _e, _a, _se, _sa, k))
         t.mine()
     for i, k in enumerate(keys):
-        casper.commit(mk_commit(i, 3, epoch_blockhash(t, 3), 2 if i == 0 else 0, k))
+        casper.commit(mk_commit(i, _e, _a, _pce[i], k))
         t.mine()
-    epoch_3_anchash = utils.sha3(epoch_blockhash(t, 3) + epoch_2_anchash)
     root_hash = t.mine().hash
-    # 4A_1: Prepare all, commit 1
+    # 4A_2: Prepare all, commit 2
     mine_epochs(t, 1)
+    _e, _a, _se, _sa, _pce = get_recommended_casper_msg_contents(casper, validator_indexes)
     for i, k in enumerate(keys):
-        casper.prepare(mk_prepare(i, 4, epoch_3_anchash, 3, epoch_3_anchash, k))
+        casper.prepare(mk_prepare(i, _e, _a, _se, _sa, k))
         t.mine()
-    casper.commit(mk_commit(0, 4, epoch_blockhash(t, 4), 3, keys[0]))
+    casper.commit(mk_commit(0, _e, _a, _pce[0], keys[0]))
+    casper.commit(mk_commit(1, _e, _a, _pce[1], keys[1]))
     chain0_4A_1 = t.mine()
     # Mine 5 more blocks to create a longer chain
     chain0_4A_1_longest = t.mine(5)
@@ -200,21 +201,23 @@ def test_head_change_to_longest_known_checkpoint_chain(db):
     t.change_head(root_hash)
     # 5B_2: Prepare all except v0, commit 2 -- Head will change
     mine_epochs(t, 2)
+    _e, _a, _se, _sa, _pce = get_recommended_casper_msg_contents(casper, validator_indexes)
     for i, k in enumerate(keys[1:], 1):
-        casper.prepare(mk_prepare(i, 5, epoch_3_anchash, 3, epoch_3_anchash, k))
+        casper.prepare(mk_prepare(i, _e, _a, _se, _sa, k))
         t.mine()
-    casper.commit(mk_commit(1, 5, epoch_blockhash(t, 5), 3, keys[1]))
+    casper.commit(mk_commit(1, _e, _a, _pce[1], keys[1]))
     t.mine()
     assert t.chain.head_hash == chain0_4A_1_longest.hash
-    casper.commit(mk_commit(2, 5, epoch_blockhash(t, 5), 3, keys[2]))
+    casper.commit(mk_commit(2, _e, _a, _pce[2], keys[2]))
     chain1_5B_2 = t.mine()
     # Make sure the head switches to chain1 becasue it has 7 commits as opposed to 6
     assert t.chain.head_hash == chain1_5B_2.hash
     # Now add a commit to chain0_4A_1, but make sure it's not the longest PoW chain
     t.change_head(chain0_4A_1.hash)
-    casper.commit(mk_commit(3, 4, epoch_blockhash(t, 4), 3, keys[3]))
+    _e, _a, _se, _sa, _pce = get_recommended_casper_msg_contents(casper, validator_indexes)
+    casper.commit(mk_commit(3, _e, _a, _pce[3], keys[3]))
     chain0_4A_2 = t.mine()
-    casper.commit(mk_commit(4, 4, epoch_blockhash(t, 4), 3, keys[4]))
+    casper.commit(mk_commit(4, _e, _a, _pce[4], keys[4]))
     chain0_4A_2 = t.mine()
     # Check to see that the head is in fact the longest PoW chain, not this fork with the recent commit
     assert t.chain.head_hash != chain0_4A_2.hash
