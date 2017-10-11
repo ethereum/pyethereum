f32e0cd3e2502ce85692a3a78c2b7dc08ef69624
Alex <achap5dk@gmail.com>
2017-10-10 20:05:19 -0700

    ver bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ tests_require = set(x.strip() for x in open('dev_requirements.txt'))
 # dependency links
 dependency_links = []
 
-version = '2.1.5'
+version = '2.1.6'
 
 setup(
     name="ethereum-augur-temp",
