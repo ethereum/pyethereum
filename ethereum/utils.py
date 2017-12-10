b110fb440c5419a5e5a1ed8353420b3dd2206d59
Joseph Krug <joeykrug@gmail.com>
2017-12-10 10:09:15 -0800

    fixed #840

--- ethereum/utils.py
+++ ethereum/utils.py
@@ -27,7 +27,7 @@ class Memoize:
         self.memo = {}
     def __call__(self, *args):
         if args not in self.memo:
-	    self.memo[args] = self.fn(*args)
+            self.memo[args] = self.fn(*args)
         return self.memo[args]
 
 def big_endian_to_int(x): return big_endian_int.deserialize(
