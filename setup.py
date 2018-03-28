31984805b5e90b9dae9837e87e1c6455b82c3787
Joseph Krug <joeykrug@gmail.com>
2018-03-28 23:08:13 +0000

    Bump version: 2.3.0 → 2.3.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ dependency_links = []
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see:
 # https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '2.3.0'
+version = '2.3.1'
 
 setup(
     name="ethereum",
